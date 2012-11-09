0c82f1d80e15f9353744f03516f90e3a40d5e107
Robert McGibbon <rmcgibbo@gmail.com>
2012-11-08 17:00:40 -0800

    Some work on saving

--- mdtraj/trajectory.py
+++ mdtraj/trajectory.py
@@ -1,12 +1,12 @@
 import os
 import numpy as np
+from numbers import Number
 
 from xtc import xtc
 from dcd import dcd
 from pdb import pdbfile
 from topology import Topology
-
-#__all__ == ['load', 'load_xtc', 'load_pdb', 'load_dcd', 'Trajectory']
+import io
 
 def load(filename, **kwargs):
     """Load a trajectory from a file or list of files
@@ -94,8 +94,11 @@ def load_xtc(filenames, top, **kwargs):
             xyz.append(np.array(c.coords).copy())
         
     xyz = np.array(xyz)
+
+    # TODO: Get the timestep
+    timestep = None
     
-    return Trajectory(xyz=xyz, topology=topology)
+    return Trajectory(xyz=xyz, topology=topology, timestep=timestep)
     
 
 
@@ -131,7 +134,7 @@ def load_hdf(filename):
     pass
 
 
-class Trajectory():
+class Trajectory(object):
     """
     Attributes
     ----------
@@ -145,43 +148,103 @@ class Trajectory():
 
     @property
     def n_frames(self):
-        return self.xyz.shape[0]
+        return self._xyz.shape[0]
 
     @property
     def n_atoms(self):
-        return self.xyz.shape[1]
+        return self._xyz.shape[1]
 
     @property
     def top(self):
         return self.topology
 
-    def __init__(self, xyz, topology, timestep=-1):
-        if xyz.ndim == 2:
+    @property
+    def timestep(self):
+        return self._timestep
+
+    @timestep.setter
+    def timestep(self, value):
+        if (isinstance(value, Number) and value > 0) or value is None:
+            self._timestep = value
+        else:
+            raise TypeError('Timestep must be a positive number or None')
+
+    @property
+    def xyz(self):
+        return self._xyz
+
+    @xyz.setter
+    def xyz(self, value):
+
+        #TODO control the precision of xyz
+
+        if value.ndim == 2:
             n_frames = 1
-            n_atoms, n_dims = xyz.shape
+            n_atoms, n_dims = value.shape
             assert n_dims == 3
 
-            self.xyz = xyz.reshape((n_frames, n_atoms, n_dims))
-        elif xyz.ndim == 3:
-            self.xyz = xyz
+            self._xyz = value.reshape((n_frames, n_atoms, n_dims))
+
+        elif value.ndim == 3:
+            self._xyz = value
+
         else:
-            print xyz
-            raise ValueError()
-            
+            raise ValueError('xyz is wrong shape')
+
+    def __init__(self, xyz, topology, timestep=None):
+        self.xyz = xyz
         self.topology = topology
         self.timestep = timestep
-
+        
         if not topology._numAtoms == self.n_atoms:
             raise ValueError("Number of atoms in xyz (%s) and "
                 "in topology (%s) don't match" % (self.n_atoms, topology._numAtoms))
+    
+    
+    @staticmethod
+    # im not really sure if the load function should be just a function or a method on the class
+    # so effectively, lets make it both?
+    def load(filename, **kwargs):
+        return load(filename, **kwargs)
+    
+    def save(self, filename):
+        "Save trajectory to a file"
+        # grab the extension of the filename
+        extension = os.path.splitext(filename)[1]
+        
+        savers = {#'.xtc': self.save_xtc,
+                  #'.pdb': self.save_pdb,
+                  #'.dcd': self.save_dcd,
+                  '.h5': self.save_hdf}
+
+        try:
+            saver = savers[extension]
+        except KeyError:
+            raise IOError('Sorry, no loader for filename=%s (extension=%s) '
+                          'was found. I can only load files '
+                          'with extensions in %s' % (filename, extension, savers.keys()))
+
+        # run the saver, and return whatever output it gives
+        return saver(filename)
+    
+    def save_hdf(self, filename):
+        # timestep needs to be an array for io to work
+        if self.timestep is None:
+            ts_array = np.array([-1])
+        else:
+            ts_array = np.array(self.timestep)
+
+        #TODO: control the precision of xyz
+
+        return io.saveh(filename, xyz=self.xyz, timestep=ts_array, topology=self.topology.to_bytearray())
 
     def join(self, other_trajectory):
         "Join two trajectories together"
-
         pass
-
 
 if __name__ == '__main__':
     #load('/home/robert/msmbuilder/Tutorial/native.pdbd')
     traj = load('/home/robert/msmbuilder/Tutorial/native.pdb')
     print load('/home/robert/msmbuilder/Tutorial/XTC/RUN00/frame0.xtc', top=traj).top
+
+    traj.save('file.h5')
