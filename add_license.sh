23fabba132bea86ce84638c46532408123c26f3e
Robert McGibbon <rmcgibbo@gmail.com>
2012-11-26 01:25:48 -0800

    almost

--- add_license.sh
+++ add_license.sh
@@ -1,7 +1,7 @@
-top_of_header=head -n 1 LICENSE_HEADER
+top_of_header=`head -n 1 LICENSE_HEADER`
 for i in `find . -name '*.py' -o -name '*.pyx'` # or whatever other pattern...
 do
-  if ! grep -q top_of_header $i
+  if ! grep -q "$top_of_header" $i
   then
     cat LICENSE_HEADER $i >$i.new && mv $i.new $i
   fi
