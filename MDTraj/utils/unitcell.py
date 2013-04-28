66245c3c33cf6867fb6cc43d93bf2f633586232b
Robert McGibbon <rmcgibbo@gmail.com>
2013-04-27 19:18:59 -0700

    conversion between box vectors and length/alpha/beta/gamma

---
+++ MDTraj/utils/unitcell.py
@@ -0,0 +1,141 @@
+##############################################################################
+# Imports
+##############################################################################
+
+import warnings
+import numpy as np
+
+##############################################################################
+# Functions
+##############################################################################
+
+
+def lengths_and_angles_to_box_vectors(a_length, b_length, c_length, alpha, beta, gamma):
+    """Convert from the lengths/angles of the unit cell to the box
+    vectors (Bravais vectors)
+    
+    ANGLES SHOULD BE IN RADIANS
+    
+    Parameters
+    ----------
+    a_length : scalar or np.ndarray
+        length of Bravais unit vector **a**
+    b_length : scalar or np.ndarray
+        length of Bravais unit vector **b**
+    c_length : scalar or np.ndarray
+        length of Bravais unit vector **c**
+    alpha : scalar or np.ndarray
+        angle between vectors **b** and **c**
+    beta : scalar or np.ndarray
+        angle between vectors **c** and **a**
+    gamma : scalar or np.ndarray
+        angle between vectors **a** and **b**
+
+    Returns
+    -------
+    a : np.ndarray
+        If the inputs are scalar, the vectors will one dimesninoal (length 3).
+        If the inputs are one dimension, shape=(n_frames, ), then the output 
+        will be (n_frames, 3)
+    b : np.ndarray
+        If the inputs are scalar, the vectors will one dimesninoal (length 3).
+        If the inputs are one dimension, shape=(n_frames, ), then the output 
+        will be (n_frames, 3)
+    c : np.ndarray
+        If the inputs are scalar, the vectors will one dimesninoal (length 3).
+        If the inputs are one dimension, shape=(n_frames, ), then the output 
+        will be (n_frames, 3)
+
+    Examples
+    --------
+    >>> lengths_and_angles_to_box_vectors(1, 1, 1, np.pi/2, np.pi/2, np.pi/2)
+    (array([1, 0, 0]), array([  6.12323400e-17,   1.00000000e+00,   0.00000000e+00]), array([  6.12323400e-17,   6.12323400e-17,   1.00000000e+00]))
+    
+    
+    Notes
+    -----
+    This code is adapted from gyroid, which is licensed under the BSD
+    http://pythonhosted.org/gyroid/_modules/gyroid/unitcell.html
+    """
+    if np.all(alpha > 2*np.pi) and np.all(beta > 2*np.pi) and np.all(gamma > 2*np.pi):
+        warnings.warn('All your angles were greater than 2*pi. Did you accidently give me degrees?')
+
+    a = np.array([a_length, np.zeros_like(a_length), np.zeros_like(a_length)])
+    b = np.array([b_length*np.cos(gamma), b_length*np.sin(gamma), np.zeros_like(b_length)])
+    cx = c_length*np.cos(beta)
+    cy = c_length*(np.cos(alpha) - np.cos(beta)*np.cos(gamma))
+    cz = np.sqrt(c_length*c_length - cx*cx - cy*cy)
+    c = np.array([cx,cy,cz])
+    
+    if not a.shape == b.shape == c.shape:
+        raise TypeError('Shape is messed up.')
+
+    return a.T, b.T, c.T
+
+
+def box_vectors_to_lengths_and_angles(a, b, c):
+    """Convert box vectors into the lengths and angles definining the box
+    
+    Parameters
+    ----------
+    a : np.ndarray
+        the vector defining the first edge of the periodic box (length 3), or
+        an array of this vector in multiple frames, where a[i,:] gives the
+        length 3 array of vector a in each frame of a simulation
+    b : np.ndarray
+        the vector defining the second edge of the periodic box (length 3), or
+        an array of this vector in multiple frames, where b[i,:] gives the
+        length 3 array of vector a in each frame of a simulation
+    c : np.ndarray
+        the vector defining the third edge of the periodic box (length 3), or
+        an array of this vector in multiple frames, where c[i,:] gives the
+        length 3 array of vector a in each frame of a simulation
+        
+    Examples
+    --------
+    >>> a = np.array([2,0,0], dtype=float)
+    >>> b = np.array([0,1,0], dtype=float)
+    >>> c = np.array([0,1,1], dtype=float)
+    >>> l1, l2, l3, alpha, beta, gamma = box_vectors_to_lengths_and_angles(a, b, c)
+    >>> (l1 == 2.0) and (l2 == 1.0) and (l3 == np.sqrt(2))
+    True
+    >>> np.abs(alpha - np.pi / 4) < 1e-10
+    True
+    >>> np.abs(beta - np.pi / 2) < 1e-10 and np.abs(gamma - np.pi / 2) < 1e-10
+    True
+
+    Returns
+    ----------
+    a_length : scalar or np.ndarray
+        length of Bravais unit vector **a**
+    b_length : scalar or np.ndarray
+        length of Bravais unit vector **b**
+    c_length : scalar or np.ndarray
+        length of Bravais unit vector **c**
+    alpha : scalar or np.ndarray
+        angle between vectors **b** and **c**
+    beta : scalar or np.ndarray
+        angle between vectors **c** and **a**
+    gamma : scalar or np.ndarray
+        angle between vectors **a** and **b**
+    """
+    if not a.shape == b.shape == c.shape:
+        raise TypeError('Shape is messed up.')
+    if not a.shape[-1] == 3:
+        raise TypeError('The last dimension must be length 3')
+    if not (a.ndim in [1,2]):
+        raise ValueError('vectors must be 1d or 2d (for a vectorized '
+                         'operation on multiple frames)')
+    last_dim = a.ndim-1
+    
+    a_length = np.sqrt(np.sum(a*a, axis=last_dim))
+    b_length = np.sqrt(np.sum(b*b, axis=last_dim))
+    c_length = np.sqrt(np.sum(c*c, axis=last_dim))
+        
+    # we allow 2d input, where the first dimension is the frame index
+    # so we want to do the dot product only over the last dimension
+    alpha = np.arccos(np.einsum('...i, ...i', b, c) / (b_length * c_length))
+    beta  = np.arccos(np.einsum('...i, ...i', c, a) / (c_length * a_length))
+    gamma = np.arccos(np.einsum('...i, ...i', a, b) / (a_length * b_length))
+
+    return a_length, b_length, c_length, alpha, beta, gamma
