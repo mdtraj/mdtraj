89f9e307fa42838cff6293e3ab60e6ae26ce6823
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-05-02 11:29:53 -0700

    Added RMSD code re issue #50

---
+++ MDTraj/IRMSD/rmsd_wrap.pyx
@@ -0,0 +1,57 @@
+import cython
+import numpy as np
+cimport numpy as np
+from cython.parallel cimport prange
+
+cdef extern float ls_rmsd2_aligned_T_g (const int nrealatoms, const int npaddedatoms, const int rowstride,
+                          const float* aT, const float* bT, const float G_a, const float G_b) nogil
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def rmsd_one_to_all(
+np.ndarray[np.float32_t, ndim=3, mode="c"] xyz1 not None, 
+np.ndarray[np.float32_t, ndim=3, mode="c"] xyz2 not None,
+np.ndarray[np.float32_t, ndim=1, mode="c"] g1 not None, 
+np.ndarray[np.float32_t, ndim=1, mode="c"] g2 not None, 
+int n_atoms,
+int frame
+):
+    """Calculate the RMSD of several frames to a single frame.
+
+    Parameters
+    ----------
+    xyz1 : np.ndarray, shape=(n_frames, 3, n_atoms_padded), dtype=float32
+        Coordinates of reference frame.
+    xyz2 : np.ndarray, shape=(n_frames, 3, n_atoms_padded), dtype=float32
+        Coordinates of frames to iterate over
+    g1 : np.ndarray, shape = (n_frames), dtype='float32'
+        Pre-calculated G factors (traces) for each frame in xyz1
+    g2 : np.ndarray, shape = (n_frames), dtype='float32'
+        Pre-calculated G factors (traces) for each frame in xyz2
+    n_atoms : int
+        The number of atoms in the system.
+    frame : int
+        Index of the desired reference frame in xyz1.
+
+    Returns
+    -------
+    Vector of distances of length len(xyz2)
+    """
+
+    cdef int n_frames, n_atoms_padded, true_stride
+    cdef Py_ssize_t i
+    
+    assert xyz1.shape[1] == 3
+    assert xyz2.shape[1] == 3
+    assert xyz1.shape[2] % 4 == 0
+    assert xyz2.shape[2] % 4 == 0
+    
+    n_frames = xyz1.shape[0]
+    n_atoms_padded = xyz1.shape[2]
+    true_stride = n_atoms_padded * 3
+
+    cdef np.ndarray[np.float32_t, ndim=1] distances = np.zeros(n_frames, dtype='float32')
+    
+    for i in prange(n_frames, nogil=True):
+        distances[i] = ls_rmsd2_aligned_T_g(n_atoms, n_atoms_padded, n_atoms_padded, &xyz1[i,0,0], &xyz2[frame,0,0], g1[i], g2[frame]) ** 0.5
+    return distances
