59225e2de5ac76e42c12ee402ad1f828c25276b8
Robert McGibbon <rmcgibbo@gmail.com>
2013-05-01 17:27:12 -0700

    whoops. forgot the new file

---
+++ MDTraj/test/test_xml.py
@@ -0,0 +1,13 @@
+from mdtraj.testing import get_fn, eq
+from mdtraj.trajectory import load
+
+def test_0():
+    t1 = load(get_fn('native2.xml'), top=get_fn('native2.pdb'))
+    t2 = load(get_fn('native2.pdb'))
+    
+    t1.center_coordinates()
+    t2.center_coordinates()
+    
+    yield lambda: eq(t1.xyz, t2.xyz)
+    yield lambda: eq(t1.unitcell_vectors, t2.unitcell_vectors)
+    
