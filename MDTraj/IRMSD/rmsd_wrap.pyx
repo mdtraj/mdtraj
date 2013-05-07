436eacddc63bb9d3705fa7b32b80214bcefb6f13
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-05-06 18:19:50 -0700

    Fixes for robert

--- MDTraj/IRMSD/rmsd_wrap.pyx
+++ MDTraj/IRMSD/rmsd_wrap.pyx
@@ -1,3 +1,5 @@
+from cpython cimport bool
+from mdtraj.utils.arrays import ensure_type
 import cython
 import numpy as np
 cimport numpy as np
@@ -14,7 +16,8 @@ np.ndarray[np.float32_t, ndim=3, mode="c"] xyz2 not None,
 np.ndarray[np.float32_t, ndim=1, mode="c"] g1 not None, 
 np.ndarray[np.float32_t, ndim=1, mode="c"] g2 not None, 
 int n_atoms,
-int frame
+int frame,
+bool parallel=True
 ):
     """Calculate the RMSD of several frames to a single frame.
 
@@ -32,28 +35,31 @@ int frame
         The number of atoms in the system.
     frame : int
         Index of the desired reference frame in xyz1.
+    parallel : bool, default True
+        If True, use openmp parallelization.
 
     Returns
     -------
-    Vector of distances of length len(xyz2)
+    ndarray of distances of length len(xyz2)
     """
-
-    cdef int n_frames, n_atoms_padded, true_stride
-    cdef Py_ssize_t i
     
-    if not ((xyz1.shape[1] == 3) & (xyz2.shape[1] == 3)):
-        raise(ValueError("Input arrays must have second dimension of 3, found %d and %d." % (xyz1.shape[1], xyz2.shape[1])))
+    cdef Py_ssize_t i
+    cdef int n_frames = xyz2.shape[0]
+    cdef int n_atoms_padded = xyz1.shape[2]
+    cdef int true_stride = n_atoms_padded * 3
+    
+    ensure_type(val=xyz1, dtype='float32', ndim=3, name="xyz1", shape=(n_frames, 3, None))
+    ensure_type(val=xyz2, dtype='float32', ndim=3, name="xyz2", shape=(n_frames, 3, None))
     if not ((xyz1.shape[2] % 4 == 0) & (xyz2.shape[2] % 4 == 0)):
         raise(ValueError("Input arrays must have third dimension of 4*n, found %d and %d." % (xyz1.shape[2], xyz2.shape[2])))
-    if not (xyz1.shape[2] == xyz2.shape[2]):
-        raise(ValueError("Input arrays must have same third dimension, found %d and %d." % (xyz1.shape[2], xyz2.shape[2])))
     
-    n_frames = xyz2.shape[0]
-    n_atoms_padded = xyz1.shape[2]
-    true_stride = n_atoms_padded * 3
-
     cdef np.ndarray[np.float32_t, ndim=1] distances = np.zeros(n_frames, dtype='float32')
-    
-    for i in prange(n_frames, nogil=True):
-        distances[i] = ls_rmsd2_aligned_T_g(n_atoms, n_atoms_padded, n_atoms_padded, &xyz1[frame,0,0], &xyz2[i,0,0], g1[frame], g2[i]) ** 0.5
+
+    if parallel == True:
+        for i in prange(n_frames, nogil=True):
+            distances[i] = ls_rmsd2_aligned_T_g(n_atoms, n_atoms_padded, n_atoms_padded, &xyz1[frame,0,0], &xyz2[i,0,0], g1[frame], g2[i]) ** 0.5
+    else:
+        for i in range(n_frames, nogil=True):
+            distances[i] = ls_rmsd2_aligned_T_g(n_atoms, n_atoms_padded, n_atoms_padded, &xyz1[frame,0,0], &xyz2[i,0,0], g1[frame], g2[i]) ** 0.5
+
     return distances
