fixed a units bug with netcdf reporter

MDTraj/hdf5.py
@@ -464,7 +464,7 @@ class HDF5TrajectoryFile(object):
         # (n_frames, n_atoms, 3)
         coordinates = ensure_type(coordinates, dtype=np.float32, ndim=3,
             name='coordinates', shape=(None, None, 3), can_be_none=False,
-            warn_on_cast=True, add_newaxis_on_deficient_ndim=True)
+            warn_on_cast=False, add_newaxis_on_deficient_ndim=True)
         n_frames, n_atoms, = coordinates.shape[0:2]
         time = ensure_type(time, dtype=np.float32, ndim=1,
             name='time', shape=(n_frames,), can_be_none=True,

MDTraj/netcdf.py
@@ -215,15 +215,14 @@ class NetCDFTrajectoryFile(object):
         if self._mode not in ['w', 'ws', 'a', 'as']:
             raise IOError('The file was opened in mode=%s. Writing is not allowed.' % self._mode)
 
-
-        coordinates = in_units_of(coordinates, 'nanometers')
+        coordinates = in_units_of(coordinates, 'angstroms')
         time = in_units_of(time, 'picoseconds')
-        cell_lengths = in_units_of(cell_lengths, 'nanometers')
+        cell_lengths = in_units_of(cell_lengths, 'angstroms')
         cell_angles = in_units_of(cell_angles, 'degrees')
 
         # typecheck all of the input arguments rigorously
         coordinates = ensure_type(coordinates, np.float32, 3, 'coordinates', length=None,
-            can_be_none=False, shape=(None, None, 3), warn_on_cast=True, add_newaxis_on_deficient_ndim=True)
+            can_be_none=False, shape=(None, None, 3), warn_on_cast=False, add_newaxis_on_deficient_ndim=True)
         n_frames, n_atoms = coordinates.shape[0], coordinates.shape[1]
 
         time = ensure_type(time, np.float32, 1, 'time', length=n_frames,

MDTraj/reporters/basereporter.py
@@ -41,24 +41,15 @@ except ImportError:
 ##############################################################################
 
 
-class _BaseRichReporter(object):
-    """HDF5Reporter stores a molecular dynamics trajectory in the HDF5 format.
-    The atomic positions, periodic box vectors, and time index are saved.
-
-    Example
-    -------
-    >>> simulation = Simulation(topology, system, integrator) # doctest: +SKIP
-    >>> h5_reporter = HDF5Reporter('traj.h5', 100)            # doctest: +SKIP
-    >>> simulation.reporters.append(h5_reporter)              # doctest: +SKIP
-    >>> simulation.step(10000)                                # doctest: +SKIP
-
-    >>> traj = mdtraj.trajectory.load('traj.lh5')             # doctest: +SKIP
+class _BaseReporter(object):
+    """
+    Baseclass for reporters.
     """
     backend = None
 
     def __init__(self, file, reportInterval, coordinates=True, time=True,
                  cell=True, potentialEnergy=True, kineticEnergy=True,
-                 temperature=True, velocities=False):
+                 temperature=True, velocities=False, atomSubset=None):
         """Create a rich reporter
 
         Parameters
@@ -68,18 +59,23 @@ class _BaseRichReporter(object):
             specifying the filename of a new HDF5 file
         reportInterval : int
             The interval (in time steps) at which to write frames.
-        coordinates : bool
+        coordinates : bool, default=True
             Whether to write the coordinates to the file.
-        time : bool
+        time : bool, default=True
             Whether to write the current time to the file.
-        cell : bool
+        cell : bool, default=True
             Whether to write the current unitcell dimensions to the file.
-        potentialEnergy : bool
+        potentialEnergy : bool, default=True
             Whether to write the potential energy to the file.
-        kineticEnergy : bool
+        kineticEnergy : bool, default=True
             Whether to write the kinetic energy to the file.
-        temperature : bool
+        temperature : bool, default=True
             Whether to write the instantaneous temperature to the file.
+        velocities : bool, default=False
+            Whether to write the velocities of each atom to the file
+        atomSubset : array_like, default=None
+            Only write a subset of the atoms, with these (zero based) indices
+            to the file. If None, *all* of the atoms will be written.
         """
         if isinstance(file, basestring):
             self._traj_file = self.backend(file, 'w')
@@ -103,9 +99,14 @@ class _BaseRichReporter(object):
         self._temperature = bool(temperature)
         self._velocities = bool(velocities)
         self._needEnergy = potentialEnergy or kineticEnergy or temperature
+        self._atomSubset = atomSubset
 
         if not OPENMM_IMPORTED:
             raise ImportError('OpenMM not found.')
+    
+    def _subsetTopology(self, topology):
+        pass
+
 
     def _initialize(self, simulation):
         """Deferred initialization of the reporter, which happens before
@@ -134,7 +135,6 @@ class _BaseRichReporter(object):
                 dof -= 3
             self._dof = dof
 
-        #self._traj_file.flush()
 
     def describeNextReport(self, simulation):
         """Get information about the next report this object will generate.
@@ -173,7 +173,7 @@ class _BaseRichReporter(object):
 
         kwargs = {}
         if self._coordinates:
-            kwargs['coordinates'] = state.getPositions(asNumpy=True).astype(np.float32)
+            kwargs['coordinates'] = state.getPositions(asNumpy=True)
         if self._time:
             kwargs['time'] = state.getTime()
         if self._cell:

MDTraj/test/test_reporter.py
@@ -99,4 +99,64 @@ def test_reporter():
         yield lambda: eq(cell_angles, 90*np.ones((50, 3)))
         yield lambda: eq(time, 0.002*2*(1+np.arange(50)))
 
-    yield lambda: eq(xyz, trajectory.load(hdf5file).xyz)
+    yield lambda: eq(trajectory.load(ncfile, top=get_fn('native.pdb')).xyz, trajectory.load(hdf5file).xyz)
+
+
+
+@np.testing.decorators.skipif(not HAVE_OPENMM, 'No OpenMM')
+def test_reporter_subset():
+    if not HAVE_OPENMM:
+        return
+
+    pdb = PDBFile(get_fn('native.pdb'))
+    forcefield = ForceField('amber99sbildn.xml', 'amber99_obc.xml')
+    system = forcefield.createSystem(pdb.topology, nonbondedMethod=CutoffNonPeriodic,
+        nonbondedCutoff=1.0*nanometers, constraints=HBonds, rigidWater=True)
+    integrator = LangevinIntegrator(300*kelvin, 1.0/picoseconds, 2.0*femtoseconds)
+    integrator.setConstraintTolerance(0.00001)
+
+    platform = Platform.getPlatformByName('Reference')
+    simulation = Simulation(pdb.topology, system, integrator, platform)
+    simulation.context.setPositions(pdb.positions)
+
+    simulation.context.setVelocitiesToTemperature(300*kelvin)
+
+    hdf5file = os.path.join(tempdir, 'traj.h5')
+    ncfile = os.path.join(tempdir, 'traj.nc')
+
+    atomSubset = [0,1,2,  4,5]
+
+    reporter = HDF5Reporter(hdf5file, 2, coordinates=True, time=True,
+        cell=True, potentialEnergy=True, kineticEnergy=True, temperature=True,
+                            velocities=True, atomSubset=atomSubset)
+    reporter2 = NetCDFReporter(ncfile, 2, coordinates=True, time=True,
+                               cell=True, atomSubset=atomAubset)
+
+    simulation.reporters.append(reporter)
+    simulation.reporters.append(reporter2)
+    simulation.step(100)
+
+    reporter.close()
+    reporter2.close()
+
+    with HDF5TrajectoryFile(hdf5file) as f:
+        got = f.read()
+        yield lambda: eq(got.temperature.shape, (50,))
+        yield lambda: eq(got.potentialEnergy.shape, (50,))
+        yield lambda: eq(got.kineticEnergy.shape, (50,))
+        yield lambda: eq(got.coordinates.shape, (50, 22, 3))
+        yield lambda: eq(got.velocities.shape, (50, 22, 3))
+        yield lambda: eq(got.cell_lengths, 2 * np.ones((50, 3)))
+        yield lambda: eq(got.cell_angles, 90*np.ones((50, 3)))
+        yield lambda: eq(got.time, 0.002*2*(1+np.arange(50)))
+
+        yield lambda: topology.equal(f.topology,
+                                     trajectory.load(get_fn('native.pdb')).top)
+
+    with NetCDFTrajectoryFile(ncfile) as f:
+        xyz, time, cell_lengths, cell_angles = f.read()
+        yield lambda: eq(cell_lengths, 2 * np.ones((50, 3)))
+        yield lambda: eq(cell_angles, 90*np.ones((50, 3)))
+        yield lambda: eq(time, 0.002*2*(1+np.arange(50)))
+
+    yield lambda: eq(trajectory.load(ncfile, top=get_fn('native.pdb')).xyz, trajectory.load(hdf5file).xyz)