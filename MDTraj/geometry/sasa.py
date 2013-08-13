2fb6d611617b15832c117065bacd95ab4675ec52
Robert McGibbon <rmcgibbo@gmail.com>
2013-08-12 20:24:22 -0700

    Add sasa and hbonds to geometry documentation

--- MDTraj/geometry/sasa.py
+++ MDTraj/geometry/sasa.py
@@ -56,6 +56,37 @@ def shrake_rupley(traj, probe_radius=0.14, n_sphere_points=960):
     -------
     areas : np.array, shape=(n_frames, n_atoms)
         The accessible surface area of each atom in every frame
+
+    Notes
+    -----
+    This code implements the Shrake and Rupley algorithm, with the Golden
+    Section Spiral algorithm to generate the sphere points. The basic idea
+    is to great a mesh of points representing the surface of each atom
+    (at a distance of the van der waals radius plus the probe
+    radis from the nuclei), and then count the number of such mesh points
+    that are on the molecular surface -- i.e. not within the radius of another
+    atom. Assuming that the points are evenly distributed, the number of points
+    is directly proportional to the accessible surface area (its just 4*pi*r^2
+    time the fraction of the points that are accessible).
+
+    There are a number of different ways to generate the points on the sphere --
+    possibly the best way would be to do a little "molecular dyanmics" : put the
+    points on the sphere, and then run MD where all the points repel one another
+    and wait for them to get to an energy minimum. But that sounds expensive.
+
+    This code uses the golden section spiral algorithm
+    (picture at http://xsisupport.com/2012/02/25/evenly-distributing-points-on-a-sphere-with-the-golden-sectionspiral/)
+    where you make this spiral that traces out the unit sphere and then put points
+    down equidistant along the spiral. It's cheap, but not perfect.
+
+    The gromacs utility g_sas uses a slightly different algorithm for generating
+    points on the sphere, which is based on an icosahedral tesselation.
+    roughly, the icosahedral tesselation works something like this
+    http://www.ziyan.info/2008/11/sphere-tessellation-using-icosahedron.html
+
+    References
+    ----------
+    .. [1] Shrake, A; Rupley, JA. (1973) J Mol Biol 79 (2): 351--71.
     """
     xyz = ensure_type(traj.xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
     out = np.zeros((xyz.shape[0], xyz.shape[1]), dtype=np.float32)
