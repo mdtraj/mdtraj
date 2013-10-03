76cd09661461201c5e78fed4abb2fdaa49a57e4e
Christian Schwantes <schwancr@stanford.edu>
2013-10-02 21:53:57 -0700

    added pca implementation. I also crated a folder called vectorizers. There is a lot of code, so these classes should really have their own files. Just like we have it for metrics in msmbuilder2.7

---
+++ MDTraj/vectorizers/pca.py
@@ -0,0 +1,166 @@
+
+from MDTraj.vectorizer import (BaseModeller, EstimatorMixin, 
+                               TransformerMixin, UpdateableEstimatorMixin)
+
+class PCATransformer(BaseModeller, EstimatorMixin, TransformerMixin, 
+    UpdateableEstimatorMixin):
+    """
+    Class for doing Principal Component Analysis (PCA) on any multivariate
+    dataset. PCA finds the linear combinations of input coordinates that 
+    maximize their explained variance, subject to being uncorrelated to the
+    previous.
+
+    The linear combinations can be shown to be the eigenvectors of the
+    covariance matrix, S:
+
+    S = E[ (X - mean)(X - mean)^T ]
+
+    """
+
+    def __init__(self, n_components=None):
+
+        self.n_components = n_components
+    
+        # running_corr_ is the running sum of the cross-correlation of the data
+        # with itself ( Outer(X, X) ) 
+        self.running_corr_ = None
+        # running_sum_ is the running sum of the data, for calculating the mean
+        self.running_sum_ = None
+        # total_frames_ is the number of frames that we have used in the estimator
+        self.total_frames_ = 0
+
+        # boolean telling us if we have an estimate of the PCs or not
+        self._have_estimate_ = False
+
+    
+    def fit_update(self, X):
+        """
+        Update the internal state with new data, X
+
+        Parameters
+        ----------
+        X : np.ndarray or list of np.ndarrays
+            Data to add to the estimate of the covariance matrix. This can be a list
+            of numpy arrays, or a single numpy array. Each array should be two-
+            dimensional: (n_samples, n_coordinates). Since this is PCA, the order
+            of the samples is irrelevant
+
+        Returns
+        -------
+        self
+        """
+
+        if not isinstance(X, list):
+            X = [X]
+
+        self._have_estimate = False
+        # we have updated the data, so we no longer have the PCs.
+
+        for row in X:
+
+            if not isinstance(row, np.ndarray):
+                raise RuntimeError("data must be numpy.ndarray's or a list of arrays")
+
+            shape = row.shape
+
+            if len(shape) == 1:
+                row = row.reshape((1, -1))
+
+            if len(shape) > 2:
+                raise RuntimeError("data cannot be more than two-dimensional")
+            
+            n_features = row.shape[1]
+
+            if not self.cov_mat_ is None:
+                self.running_corr_mat_ = np.zeros((n_features, n_features))
+                self.running_sum_ = np.zeros(n_features)
+
+            elif n_features != self.cov_mat_.shape[0]:
+                raise RuntimeError("data does not match the shape of the internal state.")
+
+            self.running_corr_mat_ += row.T.dot(row)
+            self.running_sum_ += row.sum(0)
+
+            self.total_samples_ += row.shape[0]
+
+        return self
+
+
+    def compute_components(self):
+        """
+        Compute the components according to the data that has been accumulated
+        with fit or fit_update.
+        """
+            
+        self.mean_ = self.running_sum_ / float(self.total_samples_)
+        cov_mat = self.running_sum / (self.total_samples_ - 1) - \
+                    np.outer(self.mean_, self.mean_)
+
+        vals, vecs = np.linalg.eigh(cov_mat)
+
+        ind = np.argsort(vals)[::-1]
+            
+        self.vals_ = vals[ind]
+        self.vecs_ = vecs[ind]
+
+        self._have_estimate_ = True
+
+
+    def transform(self, X):
+        """
+        Transform some data, X, onto the n_components principal components
+
+        Parameters
+        ----------
+        X : np.ndarray or list of np.ndarray's
+            data to project onto the top n_components. Should be a single two-
+            dimensional array (n_samples, n_coordinates) or a list of arrays
+            
+        Returns
+        -------
+        proj_X : np.ndarray or list of np.ndarray's
+            projected data
+
+        """
+
+        if self.n_components is None:
+            raise RuntimeError("need to set n_components")
+
+        if not self._have_estimate_:
+            self.compute_components()
+
+        if isinstance(X, list):
+            return_list = True
+
+        else:
+            X = [X]
+            return_list = False
+
+        top_pcs = self.vecs_[:, :self.n_components]
+
+        proj_X = []
+        for row in X:
+            if not isinstance(row, np.ndarray):
+                raise RuntimeError("data contains rows that are not np.ndarray's")
+    
+            shape = row.shape
+            if len(shape) == 1:
+                row = row.reshape((1, -1))
+
+            if len(shape) > 2:
+                raise RuntimeError("data cannot be more than two-dimensional")
+
+            n_features = row.shape[1]
+
+            if n_features != top_pcs.shape[0]:
+                raise RuntimeError("data is not the right shape")
+
+            proj_X.append(row.dot(top_pcs))
+
+
+        if return_list:
+            return proj_X
+        else:
+            return proj_X[0]
+
+
