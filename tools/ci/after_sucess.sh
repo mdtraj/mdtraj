0899165d2668dd24e6717973786627d27e927622
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 00:50:28 -0700

    python version checking

--- tools/ci/after_sucess.sh
+++ tools/ci/after_sucess.sh
@@ -1,5 +1,18 @@
-if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then echo "This is a pull request. No deployment will be done."; exit 0; fi
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0; fi
+PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
+
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then 
+    echo "This is a pull request. No deployment will be done."; exit 0
+fi
+
+if [[ "$PYTHON_VERSION" != "2.7" ]]; then
+    echo "No deploy on PYTHON_VERSION=${PYTHON_VERSION}"; exit 0
+fi
+
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+    echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
+fi
+
 
 sudo apt-get install -qq python-sphinx       # for building documentation
 sudo apt-get install -qq python-boto         # for interacting with S3
--- tools/ci/py_install.sh
+++ tools/ci/py_install.sh
@@ -1,9 +1,9 @@
-
 pip install -I git+https://github.com/pypa/pip@$pip_commit#egg=pip
 pip install -I -U setuptools
 pip install wheel
+PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 
 base_url=http://stanford.edu/~rmcgibbo/wheelhouse
-PIP_ARGS=" -I --use-wheel --find-links=$base_url/${TRAVIS_PYTHON_VERSION}/"
-pip install $PIP_ARGS -r requirements-${TRAVIS_PYTHON_VERSION}.txt
+PIP_ARGS=" -I --use-wheel --find-links=$base_url/${PYTHON_VERSION}/"
+pip install $PIP_ARGS -r requirements-${PYTHON_VERSION}.txt
 
