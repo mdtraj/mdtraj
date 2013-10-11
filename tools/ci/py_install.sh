dc070d49618af091dc5bd0a88296004f8f4e19c6
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 01:36:44 -0700

    Some stuff was happeening int eh venv, others not. Very confusing.

--- tools/ci/py_install.sh
+++ tools/ci/py_install.sh
@@ -1,10 +1,19 @@
 set -x
 
+# Remove any old distribute or setuptools
+rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/setuptools*
+rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/distribute*
+
+# Get  a new setuptools
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo $python
+# Get a bootstrap pip
+easy_install pip
+
 # Install Dependencies
 # as of pip 1.4rc2, wheel files are still being broken regularly, this is a
 # known good commit. should revert to pypi when a final release is out
 pip_commit=42102e9deaea99db08b681d06906c2945f6f95e2
-PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
+PYTHON_VERSION=`$python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 pip install -I git+https://github.com/pypa/pip@$pip_commit#egg=pip
 pip install -I -U wheel
 
