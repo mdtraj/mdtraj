5d2fb058974786e708df67ae7ceb966e6bd90ad5
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 01:51:48 -0700

    not running stuff in subshell

--- tools/ci/apt_install.sh
+++ tools/ci/apt_install.sh
@@ -1,9 +1,9 @@
-apt-get install python-software-properties git -y
-apt-add-repository ppa:fkrull/deadsnakes -y
-apt-get update
+sudo apt-get install python-software-properties git -y
+sudo apt-add-repository ppa:fkrull/deadsnakes -y
+sudo apt-get update
 
-apt-get install libfreetype6-dev libpng12-dev libhdf5-serial-dev \
+sudo apt-get install libfreetype6-dev libpng12-dev libhdf5-serial-dev \
     g++ libatlas-base-dev gfortran netcdf-bin libnetcdf-dev libffi-dev -y
 echo "apt-get install ${python}-dev"
 echo "apt-get install ${PYTHON}-dev"
-apt-get install python2.7-dev python2.7-dev python3.3-dev
+sudo apt-get install python2.7-dev python2.7-dev python3.3-dev
--- tools/ci/py_install.sh
+++ tools/ci/py_install.sh
@@ -1,24 +1,26 @@
 set -x
 
+$python -V
+
 # Remove any old distribute or setuptools
-rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/setuptools*
-rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/distribute*
+sudo rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/setuptools*
+sudo rm -rf `$python -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())'`/distribute*
 
 # Get  a new setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo $python
 # Get a bootstrap pip
-easy_install pip
+sudo easy_install pip
 
 # Install Dependencies
 # as of pip 1.4rc2, wheel files are still being broken regularly, this is a
 # known good commit. should revert to pypi when a final release is out
 pip_commit=42102e9deaea99db08b681d06906c2945f6f95e2
 PYTHON_VERSION=`$python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
-pip install -I git+https://github.com/pypa/pip@$pip_commit#egg=pip
-pip install -I -U wheel
+sudo pip install -I git+https://github.com/pypa/pip@$pip_commit#egg=pip
+sudo pip install -I -U wheel
 
 
 base_url=http://stanford.edu/~rmcgibbo/wheelhouse
 PIP_ARGS=" -I --use-wheel --find-links=$base_url/${PYTHON_VERSION}/"
-pip install $PIP_ARGS -r tools/ci/requirements-${PYTHON_VERSION}.txt
+sudo pip install $PIP_ARGS -r tools/ci/requirements-${PYTHON_VERSION}.txt
 
