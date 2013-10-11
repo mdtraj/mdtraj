53f69255fdfbde9f556fd43d2c8a974f4b25de95
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 00:53:48 -0700

    relative path

--- tools/ci/py_install.sh
+++ tools/ci/py_install.sh
@@ -5,5 +5,5 @@ PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 
 base_url=http://stanford.edu/~rmcgibbo/wheelhouse
 PIP_ARGS=" -I --use-wheel --find-links=$base_url/${PYTHON_VERSION}/"
-pip install $PIP_ARGS -r requirements-${PYTHON_VERSION}.txt
+pip install $PIP_ARGS -r tools/ci/requirements-${PYTHON_VERSION}.txt
 
