92b826c561f007ca1d9379dfa138a911a2df331a
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 01:46:54 -0700

    denugging

--- tools/ci/apt_install.sh
+++ tools/ci/apt_install.sh
@@ -5,4 +5,5 @@ apt-get update
 apt-get install libfreetype6-dev libpng12-dev libhdf5-serial-dev \
     g++ libatlas-base-dev gfortran netcdf-bin libnetcdf-dev libffi-dev -y
 echo "apt-get install ${python}-dev"
-apt-get install ${python}-dev
+echo "apt-get install ${PYTHON}-dev"
+apt-get install python2.7-dev python2.7-dev python3.3-dev
