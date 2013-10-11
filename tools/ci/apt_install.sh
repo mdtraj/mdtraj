f275e0996257c35068605d2f9dac08c8e9338403
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 01:57:17 -0700

    only the right version

--- tools/ci/apt_install.sh
+++ tools/ci/apt_install.sh
@@ -4,6 +4,5 @@ sudo apt-get update
 
 sudo apt-get install libfreetype6-dev libpng12-dev libhdf5-serial-dev \
     g++ libatlas-base-dev gfortran netcdf-bin libnetcdf-dev libffi-dev -y
-echo "apt-get install ${python}-dev"
-echo "apt-get install ${PYTHON}-dev"
-sudo apt-get install python2.7-dev python2.7-dev python3.3-dev
+sudo apt-get install ${python}-dev
+
