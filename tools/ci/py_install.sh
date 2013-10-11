ef270b9aa8ef928942d243a044b8bdd83c5f2bd4
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-11 01:00:14 -0700

    correct version of pip

--- tools/ci/py_install.sh
+++ tools/ci/py_install.sh
@@ -1,7 +1,10 @@
+set -x
+pip_commit=42102e9deaea99db08b681d06906c2945f6f95e2
+PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 pip install -I git+https://github.com/pypa/pip@$pip_commit#egg=pip
 pip install -I -U setuptools
 pip install wheel
-PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
+
 
 base_url=http://stanford.edu/~rmcgibbo/wheelhouse
 PIP_ARGS=" -I --use-wheel --find-links=$base_url/${PYTHON_VERSION}/"
