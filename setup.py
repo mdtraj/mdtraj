bb73aae57cbe82cf4a7bbbc628b88ee1489ba08e
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-13 06:22:37 -0700

    cross platform devnull

--- setup.py
+++ setup.py
@@ -133,11 +133,7 @@ def hasfunction(cc, funcname, include=None, extra_postargs=None):
             f.write('    %s;\n' % funcname)
             f.write('}\n')
             f.close()
-            # Redirect stderr to /dev/null to hide any error messages
-            # from the compiler.
-            # This will have to be changed if we ever have to check
-            # for a function on Windows.
-            devnull = open('/dev/null', 'w')
+            devnull = open(os.devnull, 'w')
             oldstderr = os.dup(sys.stderr.fileno())
             os.dup2(devnull.fileno(), sys.stderr.fileno())
             objects = cc.compile([fname], output_dir=tmpdir,
