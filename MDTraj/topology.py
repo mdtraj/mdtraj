1feecd8bd85e196be89691d5a87ca8104afa860e
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-26 15:38:31 -0700

    s/Foobar/MDTraj/g

--- MDTraj/topology.py
+++ MDTraj/topology.py
@@ -17,7 +17,7 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
 #
 # Portions of this code originate from the OpenMM molecular simulation
 # toolkit, copyright (c) 2012 Stanford University and Peter Eastman. Those
