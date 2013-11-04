bd50ea6156d550f5a963d576167278a5e0b81987
Robert McGibbon <rmcgibbo@gmail.com>
2013-11-03 16:21:48 -0800

    I created a messed-up mixture of tabs and spaces in theobald_rmsd.c. So I just ran astyle on the whole thing

--- MDTraj/rmsd/src/theobald_rmsd.c
+++ MDTraj/rmsd/src/theobald_rmsd.c
@@ -60,146 +60,127 @@
  */
 
 int quartic_equation_solve_exact(double *r1, double *r2, double *r3, double *r4,
-				 int *nr12, int *nr34,double d0,double d1,double d2, double d3, double d4)
-{
-  int nr;
-  double a0, a1, a2, a3, au0, au1, au2;
-  double x1, x2, x3, u1, R, R2, D, D2, E, E2, foo1, foo2;
+                                 int *nr12, int *nr34,double d0,double d1,double d2, double d3, double d4) {
+    int nr;
+    double a0, a1, a2, a3, au0, au1, au2;
+    double x1, x2, x3, u1, R, R2, D, D2, E, E2, foo1, foo2;
 
-  a3 = d3/d4;
-  a3 = d3/d4;
-  a2 = d2/d4;
-  a1 = d1/d4;
-  a0 = d0/d4;
+    a3 = d3/d4;
+    a3 = d3/d4;
+    a2 = d2/d4;
+    a1 = d1/d4;
+    a0 = d0/d4;
 
-  au2 = -a2;
-  au1 = (a1*a3 - 4.0*a0) ;
-  au0 = 4.0*a0*a2 - a1*a1 - a0*a3*a3;
+    au2 = -a2;
+    au1 = (a1*a3 - 4.0*a0) ;
+    au0 = 4.0*a0*a2 - a1*a1 - a0*a3*a3;
 
-  nr = solve_cubic_equation(1.0, au2, au1, au0, &x1, &x2, &x3);
+    nr = solve_cubic_equation(1.0, au2, au1, au0, &x1, &x2, &x3);
 
-  if (nr==1) u1 = x1;
-  else u1 = (x1>x3) ? x1 : x3;
+    if (nr==1) u1 = x1;
+    else u1 = (x1>x3) ? x1 : x3;
 
-  R2 = 0.25*a3*a3 + u1 - a2;
-  R = (R2>0.0) ? sqrt(R2) : 0.0;
+    R2 = 0.25*a3*a3 + u1 - a2;
+    R = (R2>0.0) ? sqrt(R2) : 0.0;
 
-  if (R != 0.0)
-    {
-      foo1 = 0.75*a3*a3 - R2 - 2.0*a2;
-      foo2 = 0.25*(4.0*a3*a2 - 8.0*a1 - a3*a3*a3) / R;
-      D2 = foo1 + foo2;
-      E2 = foo1 - foo2;
-    }
-  else
-    {
-      foo1 = 0.75*a3*a3 - 2.0*a2;
-      foo2 = 2.0 * sqrt(u1*u1 - 4.0*a0);
-      D2 = foo1 + foo2;
-      E2 = foo1 - foo2;
+    if (R != 0.0) {
+        foo1 = 0.75*a3*a3 - R2 - 2.0*a2;
+        foo2 = 0.25*(4.0*a3*a2 - 8.0*a1 - a3*a3*a3) / R;
+        D2 = foo1 + foo2;
+        E2 = foo1 - foo2;
+    } else {
+        foo1 = 0.75*a3*a3 - 2.0*a2;
+        foo2 = 2.0 * sqrt(u1*u1 - 4.0*a0);
+        D2 = foo1 + foo2;
+        E2 = foo1 - foo2;
     }
 
-  if (D2 >= 0.0)
-    {
-      D = sqrt(D2);
-      *r1 = -0.25*a3 + 0.5*R - 0.5*D;
-      *r2 = -0.25*a3 + 0.5*R + 0.5*D;
-      *nr12 = 2;
-    }
-  else
-    {
-      *r1 = *r2 = -0.25*a3 + 0.5*R;
-      *nr12 = 0;
+    if (D2 >= 0.0) {
+        D = sqrt(D2);
+        *r1 = -0.25*a3 + 0.5*R - 0.5*D;
+        *r2 = -0.25*a3 + 0.5*R + 0.5*D;
+        *nr12 = 2;
+    } else {
+        *r1 = *r2 = -0.25*a3 + 0.5*R;
+        *nr12 = 0;
     }
 
-  if (E2 >= 0.0)
-    {
-      E = sqrt(E2);
-      *r3 = -0.25*a3 - 0.5*R - 0.5*E;
-      *r4 = -0.25*a3 - 0.5*R + 0.5*E;
-      *nr34 = 2;
+    if (E2 >= 0.0) {
+        E = sqrt(E2);
+        *r3 = -0.25*a3 - 0.5*R - 0.5*E;
+        *r4 = -0.25*a3 - 0.5*R + 0.5*E;
+        *nr34 = 2;
+    } else {
+        *r3 = *r4 = -0.25*a3 - 0.5*R;
+        *nr34 = 0;
     }
-  else
-    {
-      *r3 = *r4 = -0.25*a3 - 0.5*R;
-      *nr34 = 0;
-    }
-  return *nr12 + *nr34;
+    return *nr12 + *nr34;
 }
 
 int solve_cubic_equation(double  c3, double  c2,  double c1, double c0,
-                         double *x1, double *x2, double *x3)
-{
-  double s1, s2;
-  double a2 = c2/c3;
-  double a1 = c1/c3;
-  double a0 = c0/c3;
+                         double *x1, double *x2, double *x3) {
+    double s1, s2;
+    double a2 = c2/c3;
+    double a1 = c1/c3;
+    double a0 = c0/c3;
 
-  double q = a1/3.0 - a2*a2/9.0;
-  double r = (a1*a2 - 3.0*a0)/6.0 - a2*a2*a2 / 27.0;
-  double delta = q*q*q + r*r;
+    double q = a1/3.0 - a2*a2/9.0;
+    double r = (a1*a2 - 3.0*a0)/6.0 - a2*a2*a2 / 27.0;
+    double delta = q*q*q + r*r;
 
-  if (delta>0.0)
-    {
-      s1 = r + sqrt(delta);
-      s1 = (s1>=0.0) ? pow(s1,1./3.) : -pow(-s1,1./3.);
+    if (delta>0.0) {
+        s1 = r + sqrt(delta);
+        s1 = (s1>=0.0) ? pow(s1,1./3.) : -pow(-s1,1./3.);
 
-      s2 = r - sqrt(delta);
-      s2 = (s2>=0.0) ? pow(s2,1./3.) : -pow(-s2,1./3.);
+        s2 = r - sqrt(delta);
+        s2 = (s2>=0.0) ? pow(s2,1./3.) : -pow(-s2,1./3.);
 
-      *x1 = (s1+s2) - a2/3.0;
-      *x2 = *x3 = -0.5 * (s1+s2) - a2/3.0;
+        *x1 = (s1+s2) - a2/3.0;
+        *x2 = *x3 = -0.5 * (s1+s2) - a2/3.0;
 
-      return 1;
-    }
-  else if (delta < 0.0)
-    {
-      double theta = acos(r/sqrt(-q*q*q)) / 3.0;
-      double costh = cos(theta);
-      double sinth = sin(theta);
-      double sq = sqrt(-q);
+        return 1;
+    } else if (delta < 0.0) {
+        double theta = acos(r/sqrt(-q*q*q)) / 3.0;
+        double costh = cos(theta);
+        double sinth = sin(theta);
+        double sq = sqrt(-q);
 
-      *x1 = 2.0*sq*costh - a2/3.0;
-      *x2 = -sq*costh - a2/3.0 - sqrt(3.) * sq * sinth;
-      *x3 = -sq*costh - a2/3.0 + sqrt(3.) * sq * sinth;
+        *x1 = 2.0*sq*costh - a2/3.0;
+        *x2 = -sq*costh - a2/3.0 - sqrt(3.) * sq * sinth;
+        *x3 = -sq*costh - a2/3.0 + sqrt(3.) * sq * sinth;
 
-      return 3;
-    }
-  else
-    {
-      double s = (r>=0.0) ? pow(r,1./3.) : -pow(-r,1./3.);
-      *x1 = 2.0*s - a2/3.0;
-      *x2 = *x3 = -s - a2/3.0;
+        return 3;
+    } else {
+        double s = (r>=0.0) ? pow(r,1./3.) : -pow(-r,1./3.);
+        *x1 = 2.0*s - a2/3.0;
+        *x2 = *x3 = -s - a2/3.0;
 
-      return 3;
+        return 3;
     }
 }
 
 
 
-float DirectSolve(float lambda, float C_0, float C_1, float C_2)
-{
-  double result;
-  double r1,r2,r3,r4;
-  int nr1,nr2;
-  quartic_equation_solve_exact(&r1,&r2,&r3,&r4,&nr1,&nr2,(double )C_0,(double)C_1,(double)C_2,0.0,1.0);
-  result=max(r1,r2);
-  result=max(result,r3);
-  result=max(result,r4);
+float DirectSolve(float lambda, float C_0, float C_1, float C_2) {
+    double result;
+    double r1,r2,r3,r4;
+    int nr1,nr2;
+    quartic_equation_solve_exact(&r1,&r2,&r3,&r4,&nr1,&nr2,(double )C_0,(double)C_1,(double)C_2,0.0,1.0);
+    result=max(r1,r2);
+    result=max(result,r3);
+    result=max(result,r4);
 
-  return (float) result;
+    return (float) result;
 }
 
-float NewtonSolve(float lambda, float C_0, float C_1, float C_2)
-{
-  unsigned int i;
-  unsigned int maxits = 500;
-  float tolerance = 1.0e-6f;
-  float lambda_old,lambda2;
-  float a,b;
+float NewtonSolve(float lambda, float C_0, float C_1, float C_2) {
+    unsigned int i;
+    unsigned int maxits = 500;
+    float tolerance = 1.0e-6f;
+    float lambda_old,lambda2;
+    float a,b;
 
-  for (i = 0; i < maxits; i++)
-    {
+    for (i = 0; i < maxits; i++) {
         lambda_old = lambda;
         lambda2 = lambda_old * lambda_old;
         b = (lambda2 + C_2) * lambda_old;
@@ -207,23 +188,21 @@ float NewtonSolve(float lambda, float C_0, float C_1, float C_2)
         lambda = lambda_old - (a * lambda_old + C_0) / (2.0f * lambda2 * lambda_old + b + a);
         if (fabsf(lambda - lambda_old) < fabsf(tolerance * lambda)) break;
     }
-  if (fabsf(lambda - lambda_old) >= fabsf(100*tolerance * lambda))
-    {
-      printf("RMSD Warning: No convergence after %d iterations: Lambda,Lambda0,Diff,Allowed = %f, %f, %f, %f \n",maxits,lambda, lambda_old, fabsf(lambda - lambda_old), fabsf(tolerance * lambda) );
+    if (fabsf(lambda - lambda_old) >= fabsf(100*tolerance * lambda)) {
+        printf("RMSD Warning: No convergence after %d iterations: Lambda,Lambda0,Diff,Allowed = %f, %f, %f, %f \n",maxits,lambda, lambda_old, fabsf(lambda - lambda_old), fabsf(tolerance * lambda) );
     }
 
-  return(lambda);
+    return(lambda);
 }
 
 float msdFromMandG(const float M[9], const float G_x, const float G_y,
-                   const int numAtoms, int computeRot, float rot[9])
-{
-	/* Compute the RMSD and (optionally) the rotation matrix from M and G. Core routine of the
-	   theobald QCP method.
-	
-	when computeRot == 0, the rotation matrix will not be computed.
-	otherwise, it will be computed and stored in `rot`.
-	*/
+                   const int numAtoms, int computeRot, float rot[9]) {
+    /* Compute the RMSD and (optionally) the rotation matrix from M and G. Core routine of the
+       theobald QCP method.
+
+    when computeRot == 0, the rotation matrix will not be computed.
+    otherwise, it will be computed and stored in `rot`.
+    */
     int i;
     const int m = 3;
     float k00 =  M[0+0*m ] + M[1+1*m] + M[2+2*m];       /* [0, 0] */
@@ -236,11 +215,11 @@ float msdFromMandG(const float M[9], const float G_x, const float G_y,
     float k22 = -M[0+0*m ] + M[1+1*m] - M[2+2*m];       /* [2, 2] */
     float k23 =  M[1+2*m ] + M[2+1*m];                  /* [2, 3] */
     float k33 = -M[0+0*m ] - M[1+1*m] + M[2+2*m];       /* [3, 3] */
-    
+
     /* declarations required for rotation code */
     float k2233_2323, k1233_1323, k1223_1322, k0223_0322, k0233_0323, k0213_0312;
     float q0, q1, q2, q3, qsqr, normq, a2, x2, y2, z2, xy, az, zx, ay, yz, ax;
-         
+
     /* float C_4 = 1.0, C_3 = 0.0; */
     float detM = 0.0f, detK = 0.0f;
     float C_2, C_1, C_0;
@@ -249,8 +228,7 @@ float msdFromMandG(const float M[9], const float G_x, const float G_y,
 
     float rmsd2,ls_rmsd2;
     C_2 = 0.0f;
-    for (i = 0; i < m * m; i++)
-    {
+    for (i = 0; i < m * m; i++) {
         C_2 += M[i] * M[i];
     }
     C_2 *= -2.0f;
@@ -259,15 +237,15 @@ float msdFromMandG(const float M[9], const float G_x, const float G_y,
     /* could use rule of Sarrus, but better: */
     /* computationally more efficient with Laplace expansion */
     detM = M[0] * (M[4] * M[8] - M[5] * M[7])
-         + M[3] * (M[7] * M[2] - M[8] * M[1])
-         + M[6] * (M[1] * M[5] - M[2] * M[4]);
+           + M[3] * (M[7] * M[2] - M[8] * M[1])
+           + M[6] * (M[1] * M[5] - M[2] * M[4]);
 
     detK = k01*k01*k23*k23   - k22*k33*k01*k01   + 2*k33*k01*k02*k12
-         - 2*k01*k02*k13*k23 - 2*k01*k03*k12*k23 + 2*k22*k01*k03*k13
-         + k02*k02*k13*k13   - k11*k33*k02*k02   - 2*k02*k03*k12*k13
-         + 2*k11*k02*k03*k23 + k03*k03*k12*k12   - k11*k22*k03*k03
-         - k00*k33*k12*k12   + 2*k00*k12*k13*k23 - k00*k22*k13*k13
-         - k00*k11*k23*k23   + k00*k11*k22*k33;
+           - 2*k01*k02*k13*k23 - 2*k01*k03*k12*k23 + 2*k22*k01*k03*k13
+           + k02*k02*k13*k13   - k11*k33*k02*k02   - 2*k02*k03*k12*k13
+           + 2*k11*k02*k03*k23 + k03*k03*k12*k12   - k11*k22*k03*k03
+           - k00*k33*k12*k12   + 2*k00*k12*k13*k23 - k00*k22*k13*k13
+           - k00*k11*k23*k23   + k00*k11*k22*k33;
 
 
     C_1 = -8.0f * detM;
@@ -280,65 +258,64 @@ float msdFromMandG(const float M[9], const float G_x, const float G_y,
     if (rmsd2 > 0.0f) ls_rmsd2 = rmsd2;
 
     if (computeRot != 0) {
-		/* compute the rotation matrix */
-		k00 -= lambda;
-		k11 -= lambda;
-		k22 -= lambda;
-		k33 -= lambda;
+        /* compute the rotation matrix */
+        k00 -= lambda;
+        k11 -= lambda;
+        k22 -= lambda;
+        k33 -= lambda;
 
-	    k2233_2323 = k22*k33 - k23*k23;
-	    k1233_1323 = k12*k33 - k13*k23;
-	    k1223_1322 = k12*k23 - k13*k22;
-	    k0223_0322 = k02*k23 - k03*k22;
-	    k0233_0323 = k02*k33 - k03*k23;
-	    k0213_0312 = k02*k13 - k03*k12;
-        
-	    q0 =  k11*k2233_2323 - k12*k1233_1323 + k13*k1223_1322;
-	    q1 = -k01*k2233_2323 + k12*k0233_0323 - k13*k0223_0322;
-	    q2 =  k01*k1233_1323 - k11*k0233_0323 + k13*k0213_0312;
-	    q3 = -k01*k1223_1322 + k11*k0223_0322 - k12*k0213_0312;
-		qsqr = q0*q0 + q1*q1 + q2*q2 + q3*q3;
+        k2233_2323 = k22*k33 - k23*k23;
+        k1233_1323 = k12*k33 - k13*k23;
+        k1223_1322 = k12*k23 - k13*k22;
+        k0223_0322 = k02*k23 - k03*k22;
+        k0233_0323 = k02*k33 - k03*k23;
+        k0213_0312 = k02*k13 - k03*k12;
 
-		if (qsqr < 1e-11f) {
-			fprintf(stderr, "%s UNCONVEGED ROTATION MATRIX. RETURNING IDENTITY=%d\n", __FILE__, __LINE__);
-			rot[0] = rot[4] = rot[8] = 1.0;
-			rot[1] = rot[2] = rot[3] = rot[5] = rot[6] = rot[7] = 0.0;
-		} else {
-		    normq = sqrt(qsqr);
-		    q0 /= normq;
-		    q1 /= normq;
-		    q2 /= normq;
-		    q3 /= normq;
+        q0 =  k11*k2233_2323 - k12*k1233_1323 + k13*k1223_1322;
+        q1 = -k01*k2233_2323 + k12*k0233_0323 - k13*k0223_0322;
+        q2 =  k01*k1233_1323 - k11*k0233_0323 + k13*k0213_0312;
+        q3 = -k01*k1223_1322 + k11*k0223_0322 - k12*k0213_0312;
+        qsqr = q0*q0 + q1*q1 + q2*q2 + q3*q3;
 
-		    a2 = q0 * q0;
-		    x2 = q1 * q1;
-		    y2 = q2 * q2;
-		    z2 = q3 * q3;
+        if (qsqr < 1e-11f) {
+            fprintf(stderr, "%s UNCONVEGED ROTATION MATRIX. RETURNING IDENTITY=%d\n", __FILE__, __LINE__);
+            rot[0] = rot[4] = rot[8] = 1.0;
+            rot[1] = rot[2] = rot[3] = rot[5] = rot[6] = rot[7] = 0.0;
+        } else {
+            normq = sqrt(qsqr);
+            q0 /= normq;
+            q1 /= normq;
+            q2 /= normq;
+            q3 /= normq;
 
-		    xy = q1 * q2;
-		    az = q0 * q3;
-		    zx = q3 * q1;
-		    ay = q0 * q2;
-		    yz = q2 * q3;
-		    ax = q0 * q1;
-		
-		    rot[0] = a2 + x2 - y2 - z2;
-		    rot[3] = 2 * (xy + az);
-		    rot[6] = 2 * (zx - ay);
-		    rot[1] = 2 * (xy - az);
-		    rot[4] = a2 - x2 + y2 - z2;
-		    rot[7] = 2 * (yz + ax);
-		    rot[2] = 2 * (zx + ay);
-		    rot[5] = 2 * (yz - ax);
-		    rot[8] = a2 - x2 - y2 + z2;
-		}
-	}
-	return ls_rmsd2;
+            a2 = q0 * q0;
+            x2 = q1 * q1;
+            y2 = q2 * q2;
+            z2 = q3 * q3;
+
+            xy = q1 * q2;
+            az = q0 * q3;
+            zx = q3 * q1;
+            ay = q0 * q2;
+            yz = q2 * q3;
+            ax = q0 * q1;
+
+            rot[0] = a2 + x2 - y2 - z2;
+            rot[3] = 2 * (xy + az);
+            rot[6] = 2 * (zx - ay);
+            rot[1] = 2 * (xy - az);
+            rot[4] = a2 - x2 + y2 - z2;
+            rot[7] = 2 * (yz + ax);
+            rot[2] = 2 * (zx + ay);
+            rot[5] = 2 * (yz - ax);
+            rot[8] = a2 - x2 - y2 + z2;
+        }
+    }
+    return ls_rmsd2;
 }
 
 float msd_axis_major(const int nrealatoms, const int npaddedatoms, const int rowstride,
-                     const float* aT, const float* bT, const float G_a, const float G_b)
-{
+                     const float* aT, const float* bT, const float G_a, const float G_b) {
     /*   Computes the mean-square-deviation between two centered structures in axis-major format.
      *
      *   Structure setup for this function:
@@ -381,12 +358,12 @@ float msd_axis_major(const int nrealatoms, const int npaddedatoms, const int row
      *
      *       G_b:          trace of B'B
      */
-	int niters, k;
-	__m128 xx,xy,xz,yx,yy,yz,zx,zy,zz;
+    int niters, k;
+    __m128 xx,xy,xz,yx,yy,yz,zx,zy,zz;
     __m128 ax,ay,az,b;
     __m128 t0,t1,t2;
     /* Will have 3 garbage elements at the end */
-	_ALIGNED(16) float M[12];
+    _ALIGNED(16) float M[12];
     const float* aTx = aT;
     const float* aTy = aT+rowstride;
     const float* aTz = aT+2*rowstride;
@@ -462,8 +439,7 @@ float msd_axis_major(const int nrealatoms, const int npaddedatoms, const int row
 
 float msd_atom_major(const int nrealatoms, const int npaddedatoms,
                      const float* a, const float* b, const float G_a, const float G_b,
-					 int computeRot, float rot[9])
-{
+                     int computeRot, float rot[9]) {
     /*   Computes the mean-square-deviation between two centered structures in atom-major format.
      * Structure setup for this function:
      *
@@ -503,11 +479,11 @@ float msd_atom_major(const int nrealatoms, const int npaddedatoms,
      *
      *       G_b:          trace of B'B
      *
-	 *       computeRot:   if 0, the rotation matrix will not be computed. Otherwise, it
-     *                     will be computed and stored in rot	                 
+     *       computeRot:   if 0, the rotation matrix will not be computed. Otherwise, it
+     *                     will be computed and stored in rot
      *
-	 *       rot:          output variable where the rotation matrix will be stored,
-	 *                     if computeRot != 0.
+     *       rot:          output variable where the rotation matrix will be stored,
+     *                     if computeRot != 0.
      */
     int niters, k;
     /* Will have 3 garbage elements at the end */
@@ -529,8 +505,7 @@ float msd_atom_major(const int nrealatoms, const int npaddedatoms,
     assert(npaddedatoms % 4 == 0);
 
     xx = xy = xz = yx = yy = yz = zx = zy = zz = _mm_setzero_ps();
-    for (k = 0; k < niters; k++)
-    {
+    for (k = 0; k < niters; k++) {
         aos_deinterleaved_load(b,&bx,&by,&bz);
         aos_deinterleaved_load(a,&ax,&ay,&az);
 
