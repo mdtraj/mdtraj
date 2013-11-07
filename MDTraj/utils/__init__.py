589d08cd12274a93ab0c179cf78e6d170af995ce
Robert McGibbon <rmcgibbo@gmail.com>
2013-11-06 20:21:52 -0800

    Update __init__.py

--- MDTraj/utils/__init__.py
+++ MDTraj/utils/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division
+import time
 from .delay_import import import_
 from .arrays import ensure_type
 from .unit import in_units_of
@@ -7,7 +8,7 @@ from .unitcell import (lengths_and_angles_to_box_vectors,
 
 __all__ = ["ensure_type", "import_", "in_units_of",
     "lengths_and_angles_to_box_vectors", "box_vectors_to_lengths_and_angles",
-    "ilen"]
+    "ilen", "timing"]
 
 
 def ilen(iterable):
@@ -24,3 +25,24 @@ def ilen(iterable):
         The number of elements in the iterable
     """
     return sum(1 for _ in iterable)
+
+
+class timing(object):
+    """A timing context manager
+
+    Example
+    -------
+    >>> with timing('long function'):
+    >>>    long_function()
+    long function: 0.500 seconds
+    """
+    def __init__(self, name='block'):
+        self.name = name
+    
+    def __enter__(self):
+        self.start = time.time()
+    
+    def __exit__(self, ty, val, tb):
+        end = time.time()
+        print("%s : %0.3f seconds" % (self.name, end - self.start))
+        return False
