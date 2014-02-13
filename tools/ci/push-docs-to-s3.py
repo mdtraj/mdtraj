d6db519951ad7b6355f3fbcd6916017805824f5f
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-13 00:01:12 -0800

    change docs bucket to mdtraj.org

--- tools/ci/push-docs-to-s3.py
+++ tools/ci/push-docs-to-s3.py
@@ -7,7 +7,7 @@ import mdtraj
 # on travis-ci to push the build documentation to Amazon S3.
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-BUCKET_NAME = 'mdtraj'
+BUCKET_NAME = 'mdtraj.org'
 
 bucket_name = AWS_ACCESS_KEY_ID.lower() + '-' + BUCKET_NAME
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
