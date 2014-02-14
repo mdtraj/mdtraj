d5a15a85fa94d07d545e22f0cffda6f41a0b0a4e
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-14 12:32:08 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MDTraj'
-copyright = u'2014, Robert McGibbon and contributors'
+copyright = u'2014, Stanford University and the Authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
