2ca434da8e52828e6a0839a2dd5007932194b520
Robert McGibbon <rmcgibbo@gmail.com>
2014-03-06 21:30:56 -0800

    after_success

--- tools/ci/after_sucess.sh
+++ tools/ci/after_sucess.sh
@@ -1,5 +1,5 @@
 export PATH=$HOME/miniconda/bin:$PATH
-export PATH=$HOME/miniconda/envs/$python/bin:$PATH
+source $HOME/miniconda/bin/activate $python
 
 PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 coveralls
@@ -18,8 +18,8 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
 fi
 
 # Create the docs and push them to S3
+sudo apt-get install -qq pandoc         # notebook -> rst
 conda install --yes matplotlib scikit-learn sphinx boto ipython-notebook jinja2
-sudo apt-get install -qq pandoc              # notebook -> rst
-sudo pip install runipy                      # example notebooks
+pip install runipy                      # example notebooks
 cd docs && make html && cd -
 python tools/ci/push-docs-to-s3.py
