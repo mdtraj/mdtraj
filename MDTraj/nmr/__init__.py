3d9600b7976352f8de51f2365e18bda6f0a16570
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-03-24 19:28:40 -0400

    Added reindex to nmr.__init__ to fix docs

--- MDTraj/nmr/__init__.py
+++ MDTraj/nmr/__init__.py
@@ -1,2 +1,2 @@
 from __future__ import absolute_import
-from .shift_wrappers import chemical_shifts_shiftx2, chemical_shifts_ppm, chemical_shifts_spartaplus
+from .shift_wrappers import chemical_shifts_shiftx2, chemical_shifts_ppm, chemical_shifts_spartaplus, reindex_dataframe_by_atoms
