8e8dd0d9658a0798a6b46c28ef62dbbec3a8bbf0
Robert McGibbon <rmcgibbo@gmail.com>
2014-03-23 21:57:14 -0700

    add sparta+ to .travis

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -11,3 +11,22 @@ export PATH=$HOME/miniconda/envs/$python/bin:$PATH
 
 PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
 pip install $PIP_ARGS -r tools/ci/requirements-${PYTHON_VERSION}.txt
+
+
+# Install SPARTA+ for NMR
+mkdir $HOME/external
+cd $HOME/external
+wget http://spin.niddk.nih.gov/bax/software/SPARTA+/sparta+.tar.Z
+REFERENCE_MD5="12a04ec45d9bd9e7974b218fe2353765"
+RECEIVED_MD5=$(md5sum sparta+.tar.Z | cut -d " " -f 1)
+if [$MD5_1 != $MD5_2] then
+    echo "sparta+.tar.Z md5 mismatch"
+    exit 1
+fi
+
+tar -xzvf sparta+.tar.Z
+cd SPARTA+
+./install.com
+export SPARTAP_DIR=$HOME/external/SPARTA+
+export SPARTA_DIR=$HOME/external/SPARTA+
+export PATH=$HOME/external/SPARTA+:$PATH
