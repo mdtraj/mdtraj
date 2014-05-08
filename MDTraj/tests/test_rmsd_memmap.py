b1c58208f7554ad3707d19d629f8ed5b3dfe88cf
Robert McGibbon <rmcgibbo@gmail.com>
2014-05-08 01:06:10 -0700

    OKay, this is really not too big of a deal

--- MDTraj/tests/test_rmsd_memmap.py
+++ MDTraj/tests/test_rmsd_memmap.py
@@ -1,17 +1,43 @@
+import os
 import mdtraj as md
-from mdtraj.testing import get_fn
+from mdtraj.testing import get_fn, assert_raises
 from sklearn.externals.joblib import load, dump
+
 
 def test_1():
     # https://github.com/rmcgibbo/mdtraj/issues/438
-    traj = md.load(get_fn('frame0.h5'))
-    filenames = dump(traj, 'temp')
-    print('saving %s' % ' '.join(filenames))
-    traj = load('temp', mmap_mode='r')
-    md.rmsd(traj, traj, 0) # BOOM
+    try:
+        traj = md.load(get_fn('frame0.h5'))
+        filenames = dump(traj, 'temp')
+        traj = load('temp', mmap_mode='r')
 
-    for fn in filenames:
-        os.unlink(fn)
+        # since traj isn't precentered, this requires centering
+        # the coordinates which is done inplace. but that's not possible
+        # with mmap_mode = 'r'
+        with assert_raises(ValueError):
+            md.rmsd(traj, traj, 0)
 
-if __name__ == '__main__':
-    test_1()
+        # this should work
+        traj = load('temp', mmap_mode='c')
+        md.rmsd(traj, traj, 0)
+
+    finally:
+        for fn in filenames:
+            os.unlink(fn)
+
+def test_2():
+    # https://github.com/rmcgibbo/mdtraj/issues/438
+    try:
+        traj = md.load(get_fn('frame0.h5'))
+        # precenter the coordinates
+        traj.center_coordinates()
+        filenames = dump(traj, 'temp')
+        traj = load('temp', mmap_mode='r')
+
+        # this should work, since we don't need to modify the
+        # coordinates inplace
+        md.rmsd(traj, traj, 0, precentered=True)
+
+    finally:
+        for fn in filenames:
+            os.unlink(fn)
