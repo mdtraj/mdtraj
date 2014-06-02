ef9e6391c89ada8324316ec49c8c8f9230f7deb2
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-02 12:46:49 -0400

    Updated

--- MDTraj/nmr/scalar_couplings.py
+++ MDTraj/nmr/scalar_couplings.py
@@ -36,15 +36,14 @@ from mdtraj.geometry import compute_phi
 
 
 J3_HN_HA_coefficients = {  # See full citations below in docstring references.
-"Ruterjans1999": dict(phi0=-60 * np.pi/180., A=7.90, B=-1.05, C=0.65),  # From Table 1. in paper.  
-"Bax2007": dict(phi0=-60 * np.pi/180., A=8.4, B=-1.36, C=0.33),  # From Table 1. in paper
-"Bax1997": dict(phi0=-60 * np.pi/180., A=7.09, B=-1.42, C=1.55),  # From Table 2. in paper
-}
+    "Ruterjans1999": dict(phi0=-60 * np.pi/180., A=7.90, B=-1.05, C=0.65),  # From Table 1. in paper.
+    "Bax2007": dict(phi0=-60 * np.pi/180., A=8.4, B=-1.36, C=0.33),  # From Table 1. in paper
+    "Bax1997": dict(phi0=-60 * np.pi/180., A=7.09, B=-1.42, C=1.55),  # From Table 2. in paper
+    }
 
 J3_HN_HA_uncertainties = pd.Series({  # Values in [Hz]
-"Ruterjans1999":0.25, "Bax2007":0.36, "Bax1997":0.39
-})
-
+    "Ruterjans1999": 0.25, "Bax2007": 0.36, "Bax1997": 0.39
+    })
 
 
 def _J3_function(phi, A, B, C, phi0):
@@ -52,59 +51,13 @@ def _J3_function(phi, A, B, C, phi0):
     return A * np.cos(phi + phi0) ** 2. + B * np.cos(phi + phi0) + C
 
 
-def _J3_HN_HA_from_phi(phi, model="Bax2007"):
-    """Calculate the scalar coupling between HN and H_alpha.
-    
-    Parameters
-    ----------
-    phi : np.ndarray, shape=(n_phi, n_frames)
-        2D array of phi dihedrals in RADIANS
-    model : string, optional, default="Bax2007"
-        Which scalar coupling model to use.  Must be one of Bax2007, Bax1999,
-        or Ruterjans1999
-
-    Returns
-    -------
-    J : np.ndarray, dtype=float, shape=(n_phi, n_frames)
-        Scalar couplings (J3_HN_HA, in [Hz]) of this trajectory
-
-    Notes
-    -----
-    The coefficients are taken from the references below--please cite them.  
-    
-    References
-    ----------    
-    
-    .. [1] Schmidt, J. M., Blümel, M., Löhr, F., & Rüterjans, H. 
-       "Self-consistent 3J coupling analysis for the joint calibration 
-       of Karplus coefficients and evaluation of torsion angles."
-       J. Biomol. NMR, 14, 1 1-12 (1999)
-
-    .. [2] Vögeli, B., Ying, J., Grishaev, A., & Bax, A. 
-       "Limits on variations in protein backbone dynamics from precise 
-       measurements of scalar couplings."
-       J. Am. Chem. Soc., 129(30), 9377-9385 (2007)
-
-    .. [3] Hu, J. S., & Bax, A. 
-       "Determination of ϕ and ξ1 Angles in Proteins from 13C-13C 
-       Three-Bond J Couplings Measured by Three-Dimensional Heteronuclear NMR.
-       How Planar Is the Peptide Bond?."
-       J. Am. Chem. Soc., 119(27), 6360-6368 (1997)
-   
-    """
-    if model not in J3_HN_HA_coefficients:
-        raise(KeyError("model must be one of %s" % J3_HN_HA_coefficients.keys()))
-    
-    return _J3_function(phi, **J3_HN_HA_coefficients[model])
-
-
 def compute_J3_HN_HA(traj, model="Bax2007"):
     """Calculate the scalar coupling between HN and H_alpha.
-    
+
     This function does not take into account periodic boundary conditions (it
     will give spurious results if the three atoms which make up any angle jump
-    across a PBC (are not "wholed"))    
-    
+    across a PBC (are not "wholed"))
+
     Parameters
     ----------
     traj : mdtraj.Trajectory
@@ -118,35 +71,37 @@ def compute_J3_HN_HA(traj, model="Bax2007"):
     indices : np.ndarray, shape=(n_phi, 4), dtype=int
         Atom indices (zero-based) of the phi dihedrals
     J : np.ndarray, shape=(n_phi, n_frames)
-        Scalar couplings (J3_HN_HA, in [Hz]) of this trajectory.  
-        `J[k]` corresponds to the phi dihedral associated with 
+        Scalar couplings (J3_HN_HA, in [Hz]) of this trajectory.
+        `J[k]` corresponds to the phi dihedral associated with
         atoms `indices[k]`
 
     Notes
     -----
-    
-    The coefficients are taken from the references below--please cite them.  
-    
+    The coefficients are taken from the references below--please cite them.
+
     References
-    ----------    
-    
-    .. [1] Schmidt, J. M., Blümel, M., Löhr, F., & Rüterjans, H. 
-       "Self-consistent 3J coupling analysis for the joint calibration 
+    ----------
+    .. [1] Schmidt, J. M., Blümel, M., Löhr, F., & Rüterjans, H.
+       "Self-consistent 3J coupling analysis for the joint calibration
        of Karplus coefficients and evaluation of torsion angles."
        J. Biomol. NMR, 14, 1 1-12 (1999)
 
-    .. [2] Vögeli, B., Ying, J., Grishaev, A., & Bax, A. 
-       "Limits on variations in protein backbone dynamics from precise 
+    .. [2] Vögeli, B., Ying, J., Grishaev, A., & Bax, A.
+       "Limits on variations in protein backbone dynamics from precise
        measurements of scalar couplings."
        J. Am. Chem. Soc., 129(30), 9377-9385 (2007)
 
-    .. [3] Hu, J. S., & Bax, A. 
-       "Determination of ϕ and ξ1 Angles in Proteins from 13C-13C 
+    .. [3] Hu, J. S., & Bax, A.
+       "Determination of ϕ and ξ1 Angles in Proteins from 13C-13C
        Three-Bond J Couplings Measured by Three-Dimensional Heteronuclear NMR.
        How Planar Is the Peptide Bond?."
        J. Am. Chem. Soc., 119(27), 6360-6368 (1997)
-   
+
     """
     indices, phi = compute_phi(traj)
-    J = _J3_HN_HA_from_phi(phi, model=model)
+
+    if model not in J3_HN_HA_coefficients:
+        raise(KeyError("model must be one of %s" % J3_HN_HA_coefficients.keys()))
+
+    J = _J3_function(phi, **J3_HN_HA_coefficients[model])
     return indices, J
