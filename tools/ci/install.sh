68058f9638259393cb2f86f346d101fc36510758
Robert McGibbon <rmcgibbo@gmail.com>
2014-06-02 11:35:36 -0700

    Build matrix

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -5,12 +5,9 @@ bash Miniconda-3.0.5-Linux-x86_64.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
 
-conda create --yes -n ${python} python=${python} numpy scipy pytables cython \
-    scipy pandas pip netcdf4 pyyaml nose
+conda create --yes -n ${python} --file tools/ci/requirements-conda-${python}.txt
 source activate $python
-
-PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
-pip install $PIP_ARGS -r tools/ci/requirements-${PYTHON_VERSION}.txt
+pip install $PIP_ARGS -r tools/ci/requirements-${python}.txt
 
 
 # Install SPARTA+ for NMR chemical shift predicition
