4420c18c92763e5e61bacd694cbd13ba22a85499
Robert McGibbon <rmcgibbo@gmail.com>
2014-06-02 18:26:26 -0700

    Update install.sh

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -2,6 +2,7 @@ sudo apt-get install -qq -y g++ gfortran valgrind csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
 wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
 bash Miniconda-3.0.5-Linux-x86_64.sh -b
+PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
 
