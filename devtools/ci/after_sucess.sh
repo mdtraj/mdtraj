0ba097068f816da0557e1079957a31700856760b
Robert McGibbon <rmcgibbo@gmail.com>
2014-09-05 19:17:12 -0700

    Fix

--- devtools/ci/after_sucess.sh
+++ devtools/ci/after_sucess.sh
@@ -27,8 +27,8 @@ fi
 
 # Install stuff for running the example IPython notebooks
 sudo apt-get install -qq pandoc         # notebook -> rst
-conda install --yes matplotlib scikit-learn sphinx==1.2.3 boto ipython-notebook jinja2 numpydoc
-pip install runipy==0.0.4                      # example notebooks
+conda install --yes matplotlib scikit-learn sphinx==1.2.3 boto ipython-notebook jinja2
+pip install numpydoc runipy==0.0.4                      # example notebooks
 
 # Install OpenMM for a couple of the the examples
 conda config --add channels http://conda.binstar.org/omnia
