f6f82bb1e2b06a6aaea2eb0914eb2bae0c630962
Jason Swails <jason.swails@gmail.com>
2014-09-09 08:36:45 -0400

    Update attribution.

--- MDTraj/tests/test_psf.py
+++ MDTraj/tests/test_psf.py
@@ -4,7 +4,7 @@
 # Copyright 2012-2013 Stanford University and the Authors
 #
 # Authors: Jason Swails
-# Contributors:
+# Contributors: Robert McGibbon
 #
 # MDTraj is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as
