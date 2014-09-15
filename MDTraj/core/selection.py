b110233ca358273b2aa97ad9db69168c25d54802
Matthew Harrigan <harrigan@stanford.edu>
2014-09-15 11:26:03 -0700

    Add license

--- MDTraj/core/selection.py
+++ MDTraj/core/selection.py
@@ -1,3 +1,25 @@
+# #############################################################################
+# MDTraj: A Python Library for Loading, Saving, and Manipulating
+# Molecular Dynamics Trajectories.
+# Copyright 2012-2014 Stanford University and the Authors
+#
+# Authors: Matthew Harrigan
+# Contributors:
+#
+# MDTraj is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
+# #############################################################################
+
 from pyparsing import (Word, alphas, nums, oneOf, Group, infixNotation, opAssoc,
                        Keyword, MatchFirst, ParseException,
                        Optional, quotedString)
--- MDTraj/tests/test_selection.py
+++ MDTraj/tests/test_selection.py
@@ -1,13 +1,32 @@
-__author__ = 'harrigan'
+# #############################################################################
+# MDTraj: A Python Library for Loading, Saving, and Manipulating
+# Molecular Dynamics Trajectories.
+# Copyright 2012-2014 Stanford University and the Authors
+#
+# Authors: Matthew Harrigan
+# Contributors:
+#
+# MDTraj is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
+# #############################################################################
 
+import logging
 
-# TODO: Expose this better
 import mdtraj
 import numpy as np
 from mdtraj.core.selection import SelectionParser
 from mdtraj.testing import eq, get_fn
 
-import logging
 
 # Conda v2.0.1 build py34_0 spews a bunch of DeprecationWarnings
 # from pyparsing internal code
