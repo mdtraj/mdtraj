e9c83683decc1b1583bbaa0791cd83371c9d5706
Robert McGibbon <rmcgibbo@gmail.com>
2014-09-30 23:43:08 -0700

    no https

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -4,7 +4,7 @@ sudo apt-get install -qq -y g++-multilib gcc-multilib
 
 MINICONDA=Miniconda-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget https://repo.continuum.io/miniconda/$MINICONDA
+wget http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
