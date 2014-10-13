23cdd2fd40e931116c3dbd84a38d740db4b18aba
Carlos Hernandez <cxh@stanford.edu>
2014-10-12 21:48:21 -0700

    added filter

--- MDTraj/geometry/src/neighbors.cpp
+++ MDTraj/geometry/src/neighbors.cpp
@@ -68,7 +68,7 @@ std::vector<int> _compute_neighbors(
         std::vector<int>::const_iterator qit;
         for (qit = query_indices.begin(); qit != query_indices.end(); ++qit) {
             // compute distance from haystack atom *hit to query atom *qit
-            if (hit == qit) {
+            if (*hit == *qit) {
                 continue;
             }
             float dist = 0;
