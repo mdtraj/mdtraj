18f840a30d9c6484d4cdd1a23e6567a8dc78fe85
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-11-02 20:22:17 -0500

    Temporary checkin of broken stuff

--- MDTraj/geometry/thermodynamic_properties.py
+++ MDTraj/geometry/thermodynamic_properties.py
@@ -1,6 +1,7 @@
 import numpy as np
 import mdtraj as md
 import mdtraj.utils.unit.unit_definitions as u
+from mdtraj.utils.unit import in_units_of
 
 # Units taken from http://en.wikipedia.org/wiki/Boltzmann_constant on Nov. 2.
 kB = 1.3806488E-23 * u.joule / u.kelvin
@@ -35,7 +36,6 @@ def dipole_moments(traj, charges):
 def static_dielectric(traj, charges, temperature):
     """Calculate the static dielectric constant from a trajectory.
 
-
     Parameters
     ----------
     traj : Trajectory
@@ -57,11 +57,11 @@ def static_dielectric(traj, charges, temperature):
     
     subtracted = moments - mu
     
-    dipole_variance = (subtracted * subtracted).sum(-1).mean(0) * (u.elementary_charge * u.nanometers) ** 2.  # <M^2> - <M>^2
+    dipole_variance = (subtracted * subtracted).sum(-1).mean(0) * (u.elementary_charge * u.nanometers) ** 2.  # <M*M> - <M>*<M>
     
     volume = np.mean(map(np.linalg.det, traj.unitcell_vectors)) * u.nanometers ** 3.  # Average box volume of trajectory
     
-    static_dielectric = 1.0 + dipole_variance * (4 * np.pi) / (3 * kB * temperature * volume * epsilon0)  # Eq. 2 of Fennell, 2012. J Phys Chem. B, except added epsilon0 in denominator.
+    static_dielectric = 1.0 + dipole_variance / (3 * kB * temperature * volume * epsilon0)  # Eq. 7 of Derivation of an improved simple point charge model for liquid water: SPC/A and SPC/L 
     
     return static_dielectric
 
@@ -74,3 +74,33 @@ def isothermal_compressability_kappa_T():
 
 def thermal_expansion_alpha_P():
     raise(NotImplementedError("This has not been implemented yet!"))
+
+
+def density(traj):
+    """Calculate the instantaneous mass density of a trajectory.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        An mdtraj trajectory.
+
+    Returns
+    -------
+    density_trace : np.array, shape=(n_frames), dtype=float
+        A timeseries of the mass density of each frame.
+    """
+    mass = sum(atom.element.mass for atom in traj.top.atoms)
+    volume_trace = np.array(map(np.linalg.det, traj.unitcell_vectors))
+    densities = mass / volume_trace
+
+    #conversion = 1.660538921E-23
+    conversion *= in_units_of(1., "nanometer ** -3", "meter ** -3")
+    conversion = in_units_of(1., "dalton * nanometer ** -3", "kilogram / item * meter ** -3")
+    
+    x = 1.0 * u.dalton
+    rho = x / (600 * u.nanometer ** 3)
+
+    rho.value_in_unit(u.gram / u.item / u.milliliter)
+
+
+    return densities * conversion
