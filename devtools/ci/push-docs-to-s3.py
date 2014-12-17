36508779ebca8eb5d14fac0f809de4a42cbf5eea
Robert McGibbon <rmcgibbo@gmail.com>
2014-12-16 19:18:28 -0800

    Fix docs push to s3

--- devtools/ci/push-docs-to-s3.py
+++ devtools/ci/push-docs-to-s3.py
@@ -1,27 +1,32 @@
 import os
-import boto
-from boto.s3.key import Key
-import mdtraj
+import pip
+import tempfile
+import subprocess
+import msmbuilder.version
+
+
+BUCKET_NAME = 'mdtraj.org'
+if not msmbuilder.version.release:
+    PREFIX = 'latest'
+else:
+    PREFIX = msmbuilder.version.short_version
+
+if not any(d.project_name == 's3cmd' for d in pip.get_installed_distributions()):
+    raise ImportError('The s3cmd pacakge is required. try $ pip install s3cmd')
 
 # The secret key is available as a secure environment variable
 # on travis-ci to push the build documentation to Amazon S3.
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-BUCKET_NAME = 'mdtraj.org'
+with tempfile.NamedTemporaryFile('w') as f:
+    f.write('''[default]
+access_key = {AWS_ACCESS_KEY_ID}
+secret_key = {AWS_SECRET_ACCESS_KEY}
+'''.format(**os.environ))
+    f.flush()
 
-conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
-            AWS_SECRET_ACCESS_KEY)
-bucket = conn.get_bucket(BUCKET_NAME)
-
-root = 'docs/_build'
-for dirpath, dirnames, filenames in os.walk(root):
-    for filename in filenames:
-        fn = os.path.join(dirpath, filename)
-        print 'Uploading', fn, '...'
-        k = Key(bucket)
-        if not mdtraj.version.release:
-            prefix = 'latest'
-        else:
-            prefix = mdtraj.version.short_version
-        k.key =  os.path.join(prefix, os.path.relpath(fn, root))
-        k.set_contents_from_filename(fn)
+    template = ('s3cmd --config {config} '
+                'sync doc/_build/ s3://{bucket}/{prefix}/')
+    cmd = template.format(
+            config=f.name,
+            bucket=BUCKET_NAME,
+            prefix=PREFIX)
+    subprocess.call(cmd.split())
