327a2194a9b4e95e1c0f45cb67410086d9dc4750
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-08 16:14:35 -0800

    different extension on win32

--- MDTraj/tests/test_capi.py
+++ MDTraj/tests/test_capi.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import mdtraj
 
 
@@ -6,4 +7,7 @@ def test_1():
     capi = mdtraj.capi()
     assert os.path.isdir(capi['lib_dir'])
     assert os.path.isdir(capi['include_dir'])
-    assert os.path.isfile(os.path.join(capi['lib_dir'], 'libtheobald.a'))
+    if sys.platform == 'win32':
+        assert os.path.isfile(os.path.join(capi['lib_dir'], 'libtheobald.lib'))
+    else:
+        assert os.path.isfile(os.path.join(capi['lib_dir'], 'libtheobald.a'))
