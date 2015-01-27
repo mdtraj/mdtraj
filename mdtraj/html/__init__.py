bab028eede0b718fe13f46feb2c122951ccbfa27
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-27 14:45:39 -0800

    nose?

--- mdtraj/html/__init__.py
+++ mdtraj/html/__init__.py
@@ -1,4 +1,6 @@
-from .install import enable_notebook
-from .trajectory_widget import TrajectoryView
-from .linked_heatmap import TrajectoryHeatmap
-
+try:
+    from .install import enable_notebook
+    from .trajectory_widget import TrajectoryView
+    from .linked_heatmap import TrajectoryHeatmap
+except ImportError:
+    pass
