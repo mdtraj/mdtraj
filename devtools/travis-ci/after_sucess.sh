a42681e0286cccac6480ad65650636254aee1229
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-27 13:57:17 -0800

    Rename devtools/ci to devtools/travis-ci

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -31,5 +31,5 @@ conda install --yes `cat docs/requirements.txt | xargs`
 conda list -e
 
 (cd docs && make html)
-python devtools/ci/push-docs-to-s3.py
-python devtools/ci/update-versions.py
+python devtools/travis-ci/push-docs-to-s3.py
+python devtools/travis-ci/update-versions.py
