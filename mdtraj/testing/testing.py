500dd7db76636163908c614260e5e54475f83e27
Robert McGibbon <rmcgibbo@gmail.com>
2015-01-29 22:41:58 -0800

    Fix py2 32-bit int/long issue

--- mdtraj/testing/testing.py
+++ mdtraj/testing/testing.py
@@ -130,6 +130,10 @@ def eq(o1, o2, decimal=6, err_msg=''):
     AssertionError
         If the tests fail
     """
+    if isinstance(o1, integer_types) and isinstance(o2, integer_types) and PY2:
+        eq_(long(o1), long(o2))
+        return
+
     assert (type(o1) is type(o2)), 'o1 and o2 not the same type: %s %s' % (type(o1), type(o2))
 
     if isinstance(o1, dict):
@@ -155,8 +159,6 @@ def eq(o1, o2, decimal=6, err_msg=''):
         assert_dict_equal(o1, o2, decimal=decimal)
     elif isinstance(o1, ast.AST) and isinstance(o2, ast.AST):
         eq_(ast.dump(o1), ast.dump(o2))
-    elif isinstance(o1, integer_types) and isinstance(o2, integer_types) and PY2:
-        eq_(long(o1), long(o2))
 
     # probably these are other specialized types
     # that need a special check?
