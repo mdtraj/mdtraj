de8f87b0238a5f6ea3b98dae302c1d84001aff31
Christoph Klein <christoph.t.klein@me.com>
2015-02-18 17:12:26 -0600

    rdf working; histogram is the current bottleneck; unit tests need some work

--- mdtraj/geometry/rdf.py
+++ mdtraj/geometry/rdf.py
@@ -22,7 +22,7 @@
 
 from __future__ import print_function, division
 
-from itertools import combinations
+import itertools
 
 import numpy as np
 
@@ -33,78 +33,60 @@ from mdtraj.geometry.distance import compute_distances
 __all__ = ['compute_rdf']
 
 
-def distance_pbc(x0, x1, dimensions):
-    """Vectorized distance calculationidering minimum image. """
-    d = np.abs(x0 - x1)
-    d = np.where(d > 0.5 * dimensions, dimensions - d, d)
-    return np.sqrt((d ** 2).sum(axis=-1))
-
-
-def compute_rdf(traj, pair_names=None, periodic=True, r_range=None, n_bins=100):
+def compute_rdf(traj, pair_names=None, r_range=None, bin_width=0.005,
+                periodic=True, opt=True):
     """Compute radial distribution functions for pairs in every frame.
 
     Parameters
     ----------
     traj : Trajectory
-    pair_names : array-like, optional
+    pair_names : array-like, shape=(2), optional, default=None
         Pair of atomtypes to consider.
-    r_range : array-like, optional
+    r_range : array-like, shape=(2), optional, default=(0.0, 1.0)
         Minimum and maximum radii.
-    n_bins : int, optional
-        Number of bins.
+    bin_width : int, optional, default=0.005
+        Width of the bins in nanometers.
+    opt : bool, default=True
+        Use an optimized native library to calculate the RDF.
 
     Returns
     -------
-    r : ndarray
+    r : np.ndarray, shape=(n_bins), dtype=float
         Radii values corresponding to bins.
-    g_r : ndarray
-        Distribution function values at r.
+    g_r : np.ndarray, shape=(n_bins), dtype=float
+        Radial distribution function values at r.
     """
-    n_bins = np.uint32(n_bins)
     if not r_range:
-        r_range = np.array([0.0, 8.0])
-    r_range = np.array(r_range)
-    g_r, edges = np.histogram([0], bins=n_bins, range=r_range)
+        r_range = np.array([0.0, 0.99])
+    r_range = ensure_type(r_range, dtype=np.float64, ndim=1, name='r_range',
+                          shape=(2,), warn_on_cast=False)
+    bins = np.arange(r_range[0], r_range[1], bin_width)
+    g_r, edges = np.histogram([0], bins=bins)
     g_r[0] = 0
 
-    rho = 0
-    all_box_lengths = traj.unitcell_lengths
-    for frame, xyz in enumerate(traj.xyz):
-        print(frame)
-        box_lengths = all_box_lengths[frame]
-        print(box_lengths, type(box_lengths))
-        for i, xyz_i in enumerate(xyz):
-            xyz_j = np.vstack([xyz[:i], xyz[i+1:]])
-            d = distance_pbc(xyz_i, xyz_j, box_lengths)
-            temp_g_r, _ = np.histogram(d, n_bins, r_range)
-            g_r += temp_g_r
-        rho += (i + 1) / np.sum(box_lengths)
+    if not pair_names:
+        # all-all
+        pairs = np.array(list(itertools.combinations(range(traj.n_atoms), 2)))
+    else:
+        type_a = traj.top.select("name =~ '{0}'".format(pair_names[0]))
+        type_b = traj.top.select("name =~ '{0}'".format(pair_names[1]))
+        # TODO: Probably a cleaner way to generate these pairs.
+        prod = itertools.product(type_a, type_b)
+        pairs = set()
+        for a, b in prod:
+            if a != b and (b, a) not in pairs:
+                pairs.add((a, b))
+        pairs = np.array(list(pairs))
 
-    # normalization
-    #n_atoms = traj.n_atoms * traj.n_frames
-    #rho = np.sum(n_atoms / traj.unitcell_volumes)
+    distances = compute_distances(traj, pairs, periodic=periodic, opt=opt)
+    #if opt:
+    #    pass
+    #else:
+    g_r, _ = np.histogram(distances, bins=bins)
+
+    # Normalize.
     r = 0.5 * (edges[1:] + edges[:-1])
-    V = 4./3. * np.pi * (np.power(edges[1:], 3) - np.power(edges[:-1], 3))
-    norm = rho * i
-    g_r = g_r.astype(np.float32)  # from uint32
-    g_r /= norm * V
+    V = (4 / 3) * np.pi * (np.power(edges[1:], 3) - np.power(edges[:-1], 3))
+    norm = len(pairs) * np.sum(1.0 / traj.unitcell_volumes) * V
+    g_r = g_r.astype(np.float64) / norm  # From int64.
     return r, g_r
-
-
-if __name__ == "__main__":
-    import pdb
-
-    import mdtraj as md
-    from mdtraj.testing import get_fn
-
-    traj = md.load(get_fn('water_216.lammpstrj'), top=get_fn('spc216.pdb'))
-    print('loaded')
-
-    r, g_r = compute_rdf(traj)
-
-    import matplotlib.pyplot as plt
-    plt.plot(r, g_r)
-    plt.show()
-
-
-
