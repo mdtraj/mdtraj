50e26adcc62b9c5792bb97adce7a70f912afa2ae
Christoph Klein <christoph.t.klein@me.com>
2015-03-03 21:57:38 -0600

    option to pre-compute pairs for RDF; factored out pairs

--- mdtraj/geometry/rdf.py
+++ mdtraj/geometry/rdf.py
@@ -34,17 +34,22 @@ from mdtraj.geometry.distance import compute_distances
 __all__ = ['compute_rdf']
 
 
-def compute_rdf(traj, pair_names=None, r_range=None, bin_width=0.005,
+def compute_rdf(traj, pairs=None, pair_names=None, r_range=None, bin_width=0.005,
                 periodic=True, opt=True):
     """Compute radial distribution functions for pairs in every frame.
 
     Parameters
     ----------
     traj : Trajectory
+    pairs : array-like, shape=(n, 2), optional, default=None
+        Indices of pairs of atoms to consider. This argument overrides any
+        information provided by pair_names.
     pair_names : array-like, shape=(2,), optional, default=None
         Pair of atom names to consider. Atom names are matched using the same
         regex matching employed by MDTraj's  atom selection DSL. If pair_names
-        is not provided, the RDF will be computed between all atoms.
+        is not provided, the RDF will be computed between all atoms. If pairs
+        are provided via the `pairs` argument, all information in `pair_names`
+        will be ignored.
     r_range : array-like, shape=(2,), optional, default=(0.0, 1.0)
         Minimum and maximum radii.
     bin_width : int, optional, default=0.005
@@ -63,27 +68,7 @@ def compute_rdf(traj, pair_names=None, r_range=None, bin_width=0.005,
     g_r : np.ndarray, shape=(np.diff(r_range) / bin_width - 1), dtype=float
         Radial distribution function values at r.
     """
-    if not pair_names:
-        # all-all
-        pairs = np.fromiter(chain.from_iterable(combinations(range(traj.n_atoms), 2)),
-                             dtype=np.int32, count=traj.n_atoms * (traj.n_atoms - 1))
-    elif len(pair_names) != 2:
-        raise ValueError('pair_names must contain two entries if you want to '
-                         'calculate the RDF for specific types of atoms.')
-    else:
-        type_a = [a.index for a in traj.top.atoms
-                  if (re.match(pair_names[0], a.name) is not None)]
-        type_b = [a.index for a in traj.top.atoms
-                  if (re.match(pair_names[1], a.name) is not None)]
-        non_matching_atom_names = [pair_names[i] for i, t in enumerate((type_a, type_b))
-                                   if len(t) == 0]
-        if non_matching_atom_names:
-            raise ValueError('Unable to find atoms matching the following '
-                             'selection(s): {0}'.format(non_matching_atom_names))
-        pairs = np.fromiter(chain.from_iterable((a, b) if a > b else (b, a) for (a, b) in product(type_a, type_b) if a != b),
-                            dtype=np.int32)
-    pairs = np.vstack((pairs[::2], pairs[1::2])).T
-
+    pairs = _generate_pairs(traj, pairs, pair_names)
     if not r_range:
         r_range = np.array([0.0, 1.0])
     r_range = ensure_type(r_range, dtype=np.float64, ndim=1, name='r_range',
@@ -104,3 +89,31 @@ def compute_rdf(traj, pair_names=None, r_range=None, bin_width=0.005,
     norm = len(pairs) * np.sum(1.0 / traj.unitcell_volumes) * V
     g_r = g_r.astype(np.float64) / norm  # From int64.
     return r, g_r
+
+
+def _generate_pairs(traj, pairs, pair_names):
+    """Generate pairs of atom indices. """
+    if pairs:
+        pairs = ensure_type(pairs, dtype=np.int32, ndim=2, name='pairs', shape=(None, 2))
+    else:
+        if not pair_names:
+            # all-all
+            pairs = np.fromiter(chain.from_iterable(combinations(range(traj.n_atoms), 2)),
+                                 dtype=np.int32, count=traj.n_atoms * (traj.n_atoms - 1))
+        elif len(pair_names) != 2:
+            raise ValueError('Pair_names must contain two entries if you want '
+                             'to calculate the RDF for specific types of atoms.')
+        else:
+            type_a = [a.index for a in traj.top.atoms
+                      if (re.match(pair_names[0], a.name) is not None)]
+            type_b = [a.index for a in traj.top.atoms
+                      if (re.match(pair_names[1], a.name) is not None)]
+            non_matching_atom_names = [pair_names[i] for i, t in enumerate((type_a, type_b))
+                                       if len(t) == 0]
+            if non_matching_atom_names:
+                raise ValueError('Unable to find atoms matching the following '
+                                 'selection(s): {0}'.format(non_matching_atom_names))
+            pairs = np.fromiter(chain.from_iterable((a, b) if a > b else (b, a) for (a, b) in product(type_a, type_b) if a != b),
+                                dtype=np.int32)
+        pairs = np.vstack((pairs[::2], pairs[1::2])).T
+    return pairs
