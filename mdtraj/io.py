f151271cd4201ab5afab39a0ff6345588f7557f8
schwancr <schwancr@stanford.edu>
2015-03-16 15:32:03 -0700

    changed removeNode -> remove_node to get rid of that annoying deprecation warning from pytables

--- mdtraj/io.py
+++ mdtraj/io.py
@@ -167,7 +167,7 @@ def saveh(file, *args, **kwargs):
 
     for key in namedict.keys():
         if key in current_nodes:
-            handle.removeNode('/', name=key)
+            handle.remove_node('/', name=key)
             # per discussion on github, https://github.com/rmcgibbo/mdtraj/issues/5
             # silent overwriting appears to be the desired functionality
             # raise IOError('Array already exists in file: %s' % key)
