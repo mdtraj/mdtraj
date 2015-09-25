Add test cases for new distance calculation corrected for non-orthorhombic triclinic unit cells and fix up some of the existing ones.

mdtraj/geometry/tests/test_distance.py
@@ -25,7 +25,7 @@ import itertools
 import numpy as np
 import mdtraj as md
 
-from mdtraj.testing import eq, skipif
+from mdtraj.testing import eq, skipif, get_fn, assert_allclose
 from mdtraj.geometry.distance import compute_distances, compute_displacements
 from mdtraj.geometry.distance import _displacement_mic, _displacement
 
@@ -93,7 +93,7 @@ def test_4():
     # pbcs
     box = np.array([[100, 0, 0], [0, 200, 0], [0, 0, 300]])
     box = np.zeros((N_FRAMES, 3, 3)) + box #broadcast it out
-    a = _displacement_mic(xyz, pairs, box)
+    a = _displacement_mic(xyz, pairs, box, False)
     b = _displacement(xyz, pairs)
     eq(a, b, decimal=3)
 
@@ -101,6 +101,34 @@ def test_5():
     # simple wrap around along the z axis.
     xyz = np.array([[[0.0, 0.0, 0.0], [0.0, 0.0, 2.2]]])
     box = np.eye(3,3).reshape(1,3,3)
-    result = _displacement_mic(xyz, np.array([[0,1]]), box)
+    result = _displacement_mic(xyz, np.array([[0,1]]), box, True)
     eq(result, np.array([[[0, 0, 0.2]]]))
 
+def test_6():
+    ext_ref = np.array([17.4835, 22.2418, 24.2910, 22.5505, 12.8686, 22.1090,
+                        7.4472, 22.4253, 19.8283, 20.6935]) / 10
+    _run_amber_traj('test_good.nc', ext_ref)
+
+def test_7():
+    ext_ref = np.array([30.9184, 23.9040, 25.3869, 28.0060, 25.9704, 24.6836,
+                        23.0508, 27.1983, 24.4954, 26.7448]) / 10
+    _run_amber_traj('test_bad.nc', ext_ref)
+
+def _run_amber_traj(trajname, ext_ref):
+    # Test triclinic case where simple approach in Tuckerman text does not
+    # always work
+    traj = md.load(get_fn(trajname), top=get_fn('test.parm7'))
+    distopt = md.compute_distances(traj, [[0, 9999]], opt=True)
+    distslw = md.compute_distances(traj, [[0, 9999]], opt=False)
+    dispopt = md.compute_displacements(traj, [[0, 9999]], opt=True)
+    dispslw = md.compute_displacements(traj, [[0, 9999]], opt=False)
+
+    eq(distopt, distslw, decimal=5)
+    eq(dispopt, dispslw, decimal=5)
+
+    assert_allclose(distopt.flatten(), ext_ref, atol=2e-5)
+
+    # Make sure distances from displacements are the same
+    eq(np.linalg.norm(dispopt, axis=2), distopt)
+    eq(np.linalg.norm(dispslw, axis=2), distslw)
+    eq(dispopt, dispslw, decimal=5)