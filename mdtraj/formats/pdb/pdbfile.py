a823800189784ca65f6309946a55a81844530f45
Rafal P. Wiewiora <rafwiewiora@gmail.com>
2015-11-06 13:09:51 -0500

    add CONECT bonds based on LAST model

--- mdtraj/formats/pdb/pdbfile.py
+++ mdtraj/formats/pdb/pdbfile.py
@@ -521,7 +521,7 @@ class PDBTrajectoryFile(object):
 
         # Add bonds based on CONECT records.
         connectBonds = []
-        for connect in pdb.models[0].connects:
+        for connect in pdb.models[-1].connects:
             i = connect[0]
             for j in connect[1:]:
                 if i in atomByNumber and j in atomByNumber:
