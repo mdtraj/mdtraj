51407ddea49431684cece9f74c5674a884d22f59
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-31 16:09:42 -0800

    Update install.sh

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -19,8 +19,7 @@ conda config --add channels http://conda.binstar.org/omnia
 MDTRAJ_DIR=`pwd`
 mkdir -p $HOME/external
 cd $HOME/external
-# wget http://spin.niddk.nih.gov/bax/software/SPARTA+/sparta+.tar.Z
-wget https://s3-us-west-1.amazonaws.com/mdtraj-travis-ci-cached-files/sparta%2B.tar.Z
+wget http://mdtraj.org/travis-ci-cache/sparta+.tar.Z
 REFERENCE_MD5="d4293336254f5696221db0edcc57cfed"
 RECEIVED_MD5=$(md5sum sparta+.tar.Z | cut -d " " -f 1)
 if [ $REFERENCE_MD5 != $RECEIVED_MD5 ]; then
@@ -37,37 +36,12 @@ export PATH=`pwd`/bin:$PATH
 # go back to the original directory we were in
 cd $MDTRAJ_DIR
 
-# -------------------
-
-# Shiftx2 now installed on travis-ci through conda.
-
-# Install shiftx2 for NMR chemical shift predicition
-# MDTRAJ_DIR=`pwd`
-# mkdir -p $HOME/external
-# cd $HOME/external
-# #wget http://www.shiftx2.ca/download/shiftx2-v107-linux-20120106.tgz
-# wget https://s3-us-west-1.amazonaws.com/mdtraj-travis-ci-cached-files/shiftx2-v107-linux-20120106.tgz
-# REFERENCE_MD5="4d3b23d77e773aa321af2a01ed04199a"
-# RECEIVED_MD5=$(md5sum shiftx2-v107-linux-20120106.tgz | cut -d " " -f 1)
-# if [ $REFERENCE_MD5 != $RECEIVED_MD5 ]; then
-#     echo "shiftx2-v107-linux-20120106.tgz md5 mismatch"
-#     exit 1
-# fi
-#
-# tar -xzf shiftx2-v107-linux-20120106.tgz
-# cd shiftx2-v107-linux/
-# make
-# export PATH=`pwd`:$PATH
-# # go back to the original directory we were in
-# cd $MDTRAJ_DIR
-
-# -------------------
 
 # Install ppm for NMR chemical shift predicition
 MDTRAJ_DIR=`pwd`
 mkdir -p $HOME/external
 cd $HOME/external
-wget https://s3-us-west-1.amazonaws.com/mdtraj-travis-ci-cached-files/ppm_linux_64.exe
+wget http://mdtraj.org/travis-ci-cache/ppm_linux_64.exe
 chmod a+x ppm_linux_64.exe
 REFERENCE_MD5="f3cb5681bd2769cdcfc77fe17c563ee4"
 RECEIVED_MD5=$(md5sum ppm_linux_64.exe | cut -d " " -f 1)
