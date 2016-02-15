201b61e52e1e0e58ed9e33b326b182ce5365d475
Matthew Harrigan <harrigan@stanford.edu>
2016-02-15 12:57:35 -0800

    fixup: Yes!

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -15,11 +15,11 @@ if [[ "$CONDA_PY" == "27" && "$CONDA_NPY" == "19" ]]; then
     # Create the docs and push them to S3
     # -----------------------------------
 
-    conda create -n py27 python=2.7
+    conda create -yq -n py27 python=2.7
     source activate py27
-    conda install --yes `conda build devtools/conda-recipe --output`
+    conda install -yq `conda build devtools/conda-recipe --output`
     pip install numpydoc s3cmd msmb_theme==0.3.1
-    conda install --yes `cat docs/requirements.txt | xargs`
+    conda install -yq `cat docs/requirements.txt | xargs`
 
     conda list -e
 
