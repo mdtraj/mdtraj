db700983548d1fca595de8a84a40244310476824
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-04-24 12:32:18 -0400

    Update geometry.cpp

--- mdtraj/geometry/src/geometry.cpp
+++ mdtraj/geometry/src/geometry.cpp
@@ -27,6 +27,12 @@
 #include <float.h>
 #include <vector>
 
+#ifdef _MSC_VER
+static float roundf(float x) {
+   return x >= 0.0f ? floorf(x + 0.5f) : ceilf(x - 0.5f);
+}
+#endif
+
 using std::vector;
 
 /****************************************************************************/
