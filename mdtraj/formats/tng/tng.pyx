1ed822df4d4ffec292e8d653ae7bafc93b6a95da
peastman <peastman@stanford.edu>
2016-05-31 16:08:41 -0700

    Implemented reading of TNG files

--- mdtraj/formats/tng/tng.pyx
+++ mdtraj/formats/tng/tng.pyx
@@ -1,18 +1,75 @@
 # cython: c_string_type=str, c_string_encoding=ascii
 
-cimport tnglib
+# cimport tnglib
 from libc.stdio cimport printf
 from libc.stdint cimport int64_t
 
 from libc.stdlib cimport malloc, free
 
+from math import ceil
+
 import numpy as np
 cimport numpy as np
 np.import_array()
 
+ctypedef enum tng_variable_n_atoms_flag: TNG_CONSTANT_N_ATOMS, TNG_VARIABLE_N_ATOMS
+ctypedef enum tng_function_status: TNG_SUCCESS, TNG_FAILURE, TNG_CRITICAL
+ctypedef enum tng_data_type: TNG_CHAR_DATA, TNG_INT_DATA, TNG_FLOAT_DATA, TNG_DOUBLE_DATA
+ctypedef enum tng_hash_mode: TNG_SKIP_HASH, TNG_USE_HASH
+
+cdef long long TNG_TRAJ_BOX_SHAPE = 0x0000000010000000LL
+
+cdef extern from "tng/tng_io.h":
+    ctypedef struct tng_trajectory_t:
+        pass
+
+    # open/close
+    tng_function_status tng_util_trajectory_open(
+                 const char *filename,
+                 const char mode,
+                 tng_trajectory_t *tng_data_p)
+    tng_function_status tng_util_trajectory_close(
+                tng_trajectory_t *tng_data_p)
+
+    # n particles
+    tng_function_status tng_num_particles_get(
+                 const tng_trajectory_t tng_data,
+                 int64_t *n)
+
+    # n frames
+    tng_function_status tng_num_frames_get(
+                const tng_trajectory_t tng_data,
+                 int64_t *n)
+
+    # read frames in chunks
+    tng_function_status tng_util_pos_read_range(
+                 const tng_trajectory_t tng_data,
+                 const int64_t first_frame,
+                 const int64_t last_frame,
+                 float **positions,
+                 int64_t *stride_length)
+
+    # Used to read the box shape
+    tng_function_status tng_data_vector_interval_get(
+                 const tng_trajectory_t tng_data,
+                 const int64_t block_id,
+                 const int64_t start_frame_nr,
+                 const int64_t end_frame_nr,
+                 const char hash_mode,
+                 void **values,
+                 int64_t *stride_length,
+                 int64_t *n_values_per_frame,
+                 char *type)
+
+    # Read time
+    tng_function_status tng_util_time_of_frame_get(
+                 const tng_trajectory_t tng_data,
+                 const int64_t frame_nr,
+                 double *time)
+
 
 cdef class TNGTrajectoryFile:
-    cdef tnglib.tng_trajectory_t _traj
+    cdef tng_trajectory_t _traj
     cdef const char * filename
     cdef char mode
     cdef int is_open
@@ -29,15 +86,15 @@ cdef class TNGTrajectoryFile:
 
         # TODO: TNG_CONSTANT_N_ATOMS assert that this is set
 
-        if tnglib.tng_util_trajectory_open(filename, mode, & self._traj) == 0:
+        if tng_util_trajectory_open(filename, mode, & self._traj) == 0:
             self.is_open = True
         else:
             raise Exception("something went wrong during opening.")
 
-        if tnglib.tng_num_particles_get(self._traj, & self.n_atoms) != 0:
+        if tng_num_particles_get(self._traj, & self.n_atoms) != 0:
             raise Exception("something went wrong during obtaining num particles")
         
-        if tnglib.tng_num_frames_get(self._traj, & self.tot_n_frames) != 0:
+        if tng_num_frames_get(self._traj, & self.tot_n_frames) != 0:
             raise Exception("error during len determination")
         
         self._pos = 0
@@ -49,141 +106,145 @@ cdef class TNGTrajectoryFile:
         self.close()
 
     def close(self):
-        "Close the XTC file handle"
+        "Close the TNG file handle"
         if self.is_open:
-            tnglib.tng_util_trajectory_close(& self._traj)
+            tng_util_trajectory_close(& self._traj)
             self.is_open = False
             self._pos = 0
 
-    def _read(self, int n_frames, atom_indices):
+    def _read_frame(self, atom_indices):
         if self._pos >= self.tot_n_frames:
             raise EOFError()
-        # buffer malloced by tng util function (tng_util_pos_read_range)
-        cdef float * positions = NULL
-        cdef int64_t stride_length, i, j
+        cdef int64_t stride_length, i, j, n_values_per_frame
         # Set a default frame range
-        cdef int64_t first_frame = self._pos, last_frame = self._pos + n_frames
         cdef int k
-        
-        if last_frame > self.tot_n_frames:# - 1):
-            last_frame = self.tot_n_frames #- 1
-        
-        n_frames = last_frame - first_frame #+ 1
-        
-        #printf("_read: first_frame = %i; last_frame=%i\n", first_frame, last_frame)
+        cdef char data_type
         
         # handle atom_indices
-        cdef int n_atoms_to_read
-        if atom_indices is None:
-            n_atoms_to_read = self.n_atoms
-        elif isinstance(atom_indices, slice):
-            n_atoms_to_read = len(np.arange(self.n_atoms)[atom_indices])
-
-        cdef np.ndarray[ndim = 3, dtype = np.float32_t, mode = 'c'] xyz = \
-            np.empty((n_frames, n_atoms_to_read, 3), dtype=np.float32)
+        cdef int n_atoms_to_read = len(atom_indices)
 
         # Get the positions of all particles in the requested frame range.
-        # The positions are stored in the positions array.
-        # N.B. No proper error checks.
-        # TODO: read time, step, box quantities
-        if (tnglib.tng_util_pos_read_range(self._traj, self._pos, last_frame,
-                                           & positions, & stride_length)
-                == 0):
-            # Print the positions of the wanted particle (zero based)
-            for i in range(0, n_frames, stride_length):
-                for j in range(0, self.n_atoms):
-                    for k in range(3):
-                        xyz[i, j, k] = positions[i / stride_length * self.n_atoms * 3
-                                        + j * 3 + k]
+        
+        cdef np.ndarray[ndim=2, dtype=np.float32_t, mode='c'] xyz = np.empty((n_atoms_to_read, 3), dtype=np.float32)
+        cdef float* positions = NULL
+        try:
+            if tng_util_pos_read_range(self._traj, self._pos, self._pos, &positions, &stride_length) == TNG_SUCCESS:
+                for i, atom_index in enumerate(atom_indices):
+                    for j in range(3):
+                        xyz[i, j] = positions[atom_index*3 + j]
+            else:
+                raise Exception("Error reading positions")
+        finally:
+            if positions != NULL:
+                free(positions)
+
+        # Get the time of each frame in the requested range.
+        
+        cdef double frame_time
+        if tng_util_time_of_frame_get(self._traj, self._pos, &frame_time) != TNG_SUCCESS:
+            # This file doesn't specify times.
+            time = None
         else:
-            if positions: free(positions)
-            raise Exception("Cannot read positions")
-        if positions:
-            free(positions)
-            
-        self._pos += n_frames
+            time = frame_time
 
-        return xyz
+        # Get the box shape of each frame in the requested range.
+        
+        cdef np.ndarray[ndim=2, dtype=np.float32_t, mode='c'] box = np.empty((3, 3), dtype=np.float32)
+        cdef void* box_shape = NULL
+        cdef float* float_box
+        cdef double* double_box
+        try:
+            if tng_data_vector_interval_get(self._traj, TNG_TRAJ_BOX_SHAPE, self._pos, self._pos, TNG_USE_HASH,
+                                        &box_shape, &stride_length, &n_values_per_frame, &data_type) == TNG_SUCCESS:
+                if data_type == TNG_DOUBLE_DATA:
+                    double_box = <double*>box_shape
+                    for j in range(3):
+                        for k in range(3):
+                            box[j, k] = double_box[j*3 + k]
+                else:
+                    float_box = <float*>box_shape
+                    for j in range(3):
+                        for k in range(3):
+                            box[j, k] = float_box[j*3 + k]
+            else:
+                raise Exception("Error reading box shape")
+        finally:
+            if box_shape != NULL:
+                free(box_shape)
 
-#     def read(self, n_frames=None, stride=None, atom_indices=None):
-#         """read(n_frames=None, stride=None, atom_indices=None)
-# 
-#         Read data from a TNG file
-# 
-#         Parameters
-#         ----------
-#         n_frames : int, None
-#             The number of frames you would like to read from the file.
-#             If None, all of the remaining frames will be loaded.
-#         stride : int, optional
-#             Read only every stride-th frame.
-#         atom_indices : array_like, optional
-#             If not none, then read only a subset of the atoms coordinates from the
-#             file. This may be slightly slower than the standard read because it required
-#             an extra copy, but will save memory.
-# 
-#         Returns
-#         -------
-#         xyz : np.ndarray, shape=(n_frames, n_atoms, 3), dtype=np.float32
-#             The cartesian coordinates, in nanometers
-#         time : np.ndarray, shape=(n_frames), dtype=np.float32
-#             The simulation time, in picoseconds, corresponding to each frame
-#         step : np.ndarray, shape=(n_frames), dtype=np.int32
-#             The step in the simulation corresponding to each frame
-#         box : np.ndarray, shape=(n_frames, 3, 3), dtype=np.float32
-#             The box vectors in each frame.
-# 
-#         See Also
-#         --------
-#         read_as_traj : Returns a Trajectory object
-#         """
-#         if not str(self.mode) == 'r':
-#             raise ValueError(
-#                 'read() is only available when file is opened in mode="r"')
-#         if not self.is_open:
-#             raise IOError('file must be open to read from it.')
-# 
-#         if n_frames is not None:
-#             # if they supply the number of frames they want, that's easy
-#             if not int(n_frames) == n_frames:
-#                 raise ValueError(
-#                     'n_frames must be an int, you supplied "%s"' % n_frames)
-#             xyz, time, step, box = self._read(int(n_frames), atom_indices)
-#             xyz, time, step, box = xyz[::stride], time[
-#                 ::stride], step[::stride], box[::stride]
-#             if np.all(np.logical_and(box < 1e-10, box > -1e-10)):
-#                 box = None
-#             return xyz, time, step, box
-# 
-#         # if they want ALL of the remaining frames, we need to guess at the
-#         # chunk size, and then check the exit status to make sure we're really
-#         # at the EOF
-#         all_xyz, all_time, all_step, all_box = [], [], [], []
+        self._pos += 1
 
-#         while True:
-#             # guess the size of the chunk to read, based on how many frames we
-#             # think are in the file and how many we've currently read
-#             chunk = max(abs(int((self.approx_n_frames - self.frame_counter) * self.chunk_size_multiplier)),
-#                         self.min_chunk_size)
-# 
-#             xyz, time, step, box = self._read(chunk, atom_indices)
-#             if len(xyz) <= 0:
-#                 break
-# 
-#             all_xyz.append(xyz)
-#             all_time.append(time)
-#             all_step.append(step)
-#             all_box.append(box)
-# 
-#         if len(all_xyz) == 0:
-#             return np.array([]), np.array([]), np.array([]), np.array([])
-#         all_xyz = np.concatenate(all_xyz)[::stride]
-#         all_time = np.concatenate(all_time)[::stride]
-#         all_step = np.concatenate(all_step)[::stride]
-#         all_box = np.concatenate(all_box)[::stride]
-#         if np.all(np.logical_and(all_box < 1e-10, all_box > -1e-10)):
-#             all_box = None
-#         return all_xyz, all_time, all_step, all_box
+        return xyz, time, box
+
+    def read(self, n_frames=None, stride=None, atom_indices=None):
+        """read(n_frames=None, stride=None, atom_indices=None)
+
+        Read data from a TNG file
+
+        Parameters
+        ----------
+        n_frames : int, None
+            The number of frames you would like to read from the file.
+            If None, all of the remaining frames will be loaded.
+        stride : int, optional
+            Read only every stride-th frame.
+        atom_indices : array_like, optional
+            If not none, then read only a subset of the atoms coordinates from the
+            file. This may be slightly slower than the standard read because it requires
+            an extra copy, but will save memory.
+
+        Returns
+        -------
+        xyz : np.ndarray, shape=(n_frames, n_atoms, 3), dtype=np.float32
+            The cartesian coordinates, in nanometers
+        time : np.ndarray, shape=(n_frames), dtype=np.float32
+            The simulation time, in picoseconds, corresponding to each frame
+        box : np.ndarray, shape=(n_frames, 3, 3), dtype=np.float32
+            The box vectors in each frame.
+
+        See Also
+        --------
+        read_as_traj : Returns a Trajectory object
+        """
+        if not self.mode == 'r':
+            raise ValueError(
+                'read() is only available when file is opened in mode="r"')
+        if not self.is_open:
+            raise IOError('file must be open to read from it.')
+
+        if stride is None:
+            stride = 1
+        if n_frames is None:
+            n_frames = ceil((self.tot_n_frames-self._pos)/float(stride))
+        if atom_indices is None:
+            atom_indices = np.arange(self.n_atoms)
+        elif isinstance(atom_indices, slice):
+            atom_indices = np.arange(self.n_atoms)[atom_indices]
+        else:
+            atom_indices = np.asarray(atom_indices)
+            if min(atom_indices) < 0:
+                raise ValueError('atom_indices should be zero indexed. You gave an index less than zero')
+            if max(atom_indices) >= self.n_atoms:
+                raise ValueError('atom indices should be zero indexed. You gave an index bigger than the number of atoms')
+        
+        # Read the frames one at a time.
+
+        all_xyz = np.empty((n_frames, len(atom_indices), 3))
+        all_time = []
+        all_box = np.empty((n_frames, 3, 3));
+        for i in range(n_frames):
+            xyz, time, box = self._read_frame(atom_indices)
+            all_xyz[i] = xyz
+            all_time.append(time)
+            all_box[i] = box
+            self._pos += stride-1
+        if any(time is None for time in all_time):
+            all_time = None
+        else:
+            all_time = np.array(all_time)
+        if np.all(np.logical_and(all_box < 1e-10, all_box > -1e-10)):
+            all_box = None
+        return all_xyz, all_time, all_box
 # 
 #     def _read(self, int n_frames, atom_indices):
 #         """Read a specified number of XTC frames from the buffer"""
