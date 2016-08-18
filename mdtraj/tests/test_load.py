a08de1d3c7f7dfc72c8b3b8e9019d1b7b5ad004e
Connor Brinton <connor.brinton@gmail.com>
2016-08-18 11:42:00 -0700

    Fix test for loading multiple trajectories.

--- mdtraj/tests/test_load.py
+++ mdtraj/tests/test_load.py
@@ -1,30 +1,3 @@
-##############################################################################
-# MDTraj: A Python Library for Loading, Saving, and Manipulating
-#         Molecular Dynamics Trajectories.
-# Copyright 2012-2013 Stanford University and the Authors
-#
-# Authors: Robert McGibbon
-# Contributors:
-#
-# MDTraj is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation, either version 2.1
-# of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
-
-
-"""
-Tests of generic loading functionality.
-"""
-
 from mdtraj import load
 from mdtraj.testing import get_fn
 
@@ -44,5 +17,6 @@ def test_load_many_list():
     """
     See if a multi-element list of files is successfully loaded.
     """
-    traj = load(2 * [get_fn('frame0.pdb')], discard_overlapping_frames=False)
-    assert traj.n_frames == 2
+    single = load(get_fn('frame0.pdb'))
+    double = load(2 * [get_fn('frame0.pdb')], discard_overlapping_frames=False)
+    assert 2 * single.n_frames == double.n_frames
