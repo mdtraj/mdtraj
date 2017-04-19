Fix tranform calculation and Transform object to be more general.
Currenlty, if you create a transform object with compute_tranformation,
you will get a Transform object that will only be able to correctly
transform groups of atoms that are the same as the target, i.e. you
can't align an entire system including water when you just compute
the tranformation matrix on protein.

mdtraj/geometry/alignment.py
@@ -69,8 +69,8 @@ class Transformation(object):
             xyz coordinates after being rotated and translated
 
         """
-        mu1 = coordinates.mean(0)
-        return coordinates.dot(self.rotation) + self.translation - mu1.dot(self.rotation)
+        return coordinates.dot(self.rotation) + self.translation
+        
 
     def __call__self(self, coordinates):
         return self.transform(coordinates)
@@ -138,8 +138,6 @@ def compute_translation_and_rotation(mobile, target):
     mu1 = mobile.mean(0)
     mu2 = target.mean(0)
 
-    translation = mu2
-
     mobile = mobile - mu1
     target = target - mu2
 
@@ -149,6 +147,8 @@ def compute_translation_and_rotation(mobile, target):
     if is_reflection:
         V[:, -1] = -V[:, -1]
     rotation = np.dot(V, W_tr)
+
+    translation = mu2 - mu1.dot(rotation)
 
     return translation, rotation
 