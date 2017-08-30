3eb1f8d408d45d6b29f4317f17696a11fe03a708
Matthew Harrigan <harrigan@stanford.edu>
2017-08-30 14:33:11 -0700

    Whoops ... revert skip logic for shiftx2

--- tests/test_nmr.py
+++ tests/test_nmr.py
@@ -34,8 +34,8 @@ def test_ppm(get_fn):
     #       2     ASN   CA    999.000  53.004  51.168  51.802  53.081  54.098  52.820  52.379  51.856  53.034  52.754  54.134  54.222  51.210  52.207  50.824  54.459  53.605  54.211  53.688  52.344  53.004  51.168  51.802  53.081  54.098  52.820  52.379  51.856  53.034  52.754  54.134  54.222  51.210  52.207  50.824  54.459  53.605  54.211  53.688  52.344
 
 
-@pytest.mark.skipif(not (find_executable(SHIFTX2) ),
-                    reason='SHIFTX2 binary not found.')
+@pytest.mark.skipif(not (find_executable(SHIFTX2) and six.PY2),
+                    reason='SHIFTX2 binary not found or Python 2 not found.')
 def test_shiftx2(get_fn):
     t = md.load(get_fn('2EQQ.pdb'))
     result = md.chemical_shifts_shiftx2(t)
