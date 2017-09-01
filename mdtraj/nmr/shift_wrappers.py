6c7cf597c1b9f76921c0e87f50de2955104cd837
Matthew Harrigan <harrigan@stanford.edu>
2017-09-01 11:49:34 -0700

    switch to sep=\s+ to avoid warnings

--- mdtraj/nmr/shift_wrappers.py
+++ mdtraj/nmr/shift_wrappers.py
@@ -210,7 +210,7 @@ def chemical_shifts_ppm(trj):
         if return_flag != 0:
             raise(IOError("Could not successfully execute command '%s', check your PPM installation or your input trajectory." % cmd))
 
-        d = pd.read_table("./bb_details.dat", index_col=False, header=None, sep="\s*").drop([3], axis=1)
+        d = pd.read_table("./bb_details.dat", index_col=False, header=None, sep="\s+").drop([3], axis=1)
 
         d = d.rename(columns={0: "resSeq", 1: "resName", 2: "name"})
         d["resSeq"] += first_resSeq - 1  # Fix bug in PPM that reindexes to 1
@@ -287,7 +287,7 @@ def chemical_shifts_spartaplus(trj, rename_HN=True):
 
         results = []
         for i in range(trj.n_frames):
-            d = pd.read_table("./trj%d_pred.tab" % i, names=names, header=None, sep="\s*", skiprows=lines_to_skip)
+            d = pd.read_table("./trj%d_pred.tab" % i, names=names, header=None, sep="\s+", skiprows=lines_to_skip)
             d["frame"] = i
             results.append(d)
 
