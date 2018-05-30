e8e031f454bd783c3af54443e57d837cddac4de2
Carlos Hernandez <cxh@stanford.edu>
2018-05-30 11:30:54 -0700

    add test

---
+++ tests/test_mae.py
@@ -0,0 +1,36 @@
+##############################################################################
+# MDTraj: A Python Library for Loading, Saving, and Manipulating
+#         Molecular Dynamics Trajectories.
+# Copyright 2012-2017 Stanford University and the Authors
+#
+# Authors: Robert McGibbon
+# Contributors: Jason Swails, Matthew Harrigan
+#
+# MDTraj is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+
+from __future__ import print_function, division
+from mdtraj.testing import eq
+from mdtraj import load
+
+
+def test_maeread(get_fn):
+    pdb = get_fn('native.pdb')
+    p = load(pdb)
+
+    mae = get_fn('native.mae')
+    m = load(mae)
+
+    assert eq(p, m)
