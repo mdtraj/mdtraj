986587269d2ce96e26117116709e52617c765b0b
marscher <m.scherer@fu-berlin.de>
2019-05-06 13:34:47 +0200

    pip ignore installed=false

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -3,8 +3,8 @@ if [[ ${MDTRAJ_TEST_PEP517} = "YES" ]]; then
     echo "testing for PEP517, enable pip installing dependencies"
     export PIP_NO_INDEX=false
     export PIP_NO_DEPENDENCIES=false
-    #export PIP_IGNORE_INSTALLED=false
+    export PIP_IGNORE_INSTALLED=false
 fi
 
 env | sort
-pip install -v .
+pip install .
