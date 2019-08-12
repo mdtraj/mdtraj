25d0ae800eb298537ffd8bd9ab0ac3541ad756cd
Kirill Shmilovich <kshmilov@gmail.com>
2019-08-11 22:08:51 -0500

    Added shape metrics, added tests, mentioned in docs

--- mdtraj/geometry/__init__.py
+++ mdtraj/geometry/__init__.py
@@ -31,7 +31,8 @@ __all__ = ['baker_hubbard', 'shrake_rupley', 'kabsch_sander', 'compute_distances
            'wernet_nilsson', 'compute_dssp', 'compute_neighbors',
            'compute_neighborlist', 'compute_rdf', 'compute_nematic_order',
            'compute_inertia_tensor', 'compute_gyration_tensor', 'find_closest_contact',
-           'compute_directors',
+           'compute_directors', 'principal_moments', 'asphericity', 'acylindricity', 
+           'relative_shape_antisotropy',
 
            # from thermodynamic_properties
            'dipole_moments', 'static_dielectric', 'isothermal_compressability_kappa_T',
@@ -52,3 +53,4 @@ from .neighborlist import *
 from .thermodynamic_properties import *
 from .rdf import *
 from .order import *
+from .shape import *
--- mdtraj/geometry/shape.py
+++ mdtraj/geometry/shape.py
@@ -26,7 +26,7 @@ import numpy as np
 
 from mdtraj.geometry.distance import compute_center_of_geometry
 
-__all__ = ['compute_gyration_tensor']
+__all__ = ['compute_gyration_tensor', 'principal_moments', 'asphericity', 'acylindricity', 'relative_shape_antisotropy']
 
 
 def compute_gyration_tensor(traj):
@@ -57,6 +57,99 @@ def compute_gyration_tensor(traj):
     xyz = traj.xyz - center_of_geom
     return np.einsum('...ji,...jk->...ik', xyz, xyz) / traj.n_atoms
 
+def principal_moments(traj):
+    """Compute the principal moments of a trajectory.
+
+    For each frame calculate the eigenvalues of the gyration tensor.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute gyration tensor of.
+
+    Returns
+    -------
+    evecs:  np.ndarray, shape=(traj.n_frames, 3), dtype=float64
+        Principal moments for each frame in ascending order.
+
+    """
+    gyration_tensor = compute_gyration_tensor(traj)
+    return np.linalg.eigvalsh(gyration_tensor)
+
+def asphericity(traj):
+    """Compute the asphericity of a trajectory.
+
+    For each frame compute the principal moments then,
+
+    .. math::
+
+        b = \frac{1}{2}(\lambda_1^2 + \lambda_2^2)
+
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute gyration tensor of.
+
+    Returns
+    -------
+    b:  np.ndarray, shape=(traj.n_frames, 1), dtype=float64
+        Asphericity of each frame of the trajectory.
+
+    """
+    pm = principal_moments(traj)
+    b = pm[:,2] - (pm[:,0] + pm[:,1]) / 2.0
+    return b
+
+def acylindricity(traj):
+    """Compute the acylindricity of a trajectory.
+
+    For each frame compute the principal moments then,
+
+    .. math::
+
+        c = \lambda_2^2 - \lambda_1^2
+
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute gyration tensor of.
+
+    Returns
+    -------
+    c:  np.ndarray, shape=(traj.n_frames, 1), dtype=float64
+        Acylindricity of each frame of the trajectory.
+
+    """
+    pm = principal_moments(traj)
+    c = pm[:,1] - pm[:,0]
+    return c
+
+def relative_shape_antisotropy(traj):
+    """Compute the acylindricity of a trajectory.
+
+    For each frame compute the principal moments then,
+
+    .. math::
+
+        \kappa^2 = \frac{3}{2}\frac{\lambda_1^4 + \lambda_2^4 + \lambda_3^4}{(\lambda_1^2 + \lambda_2^2 + \lambda_3^2)^2} - \frac{1}{2}
+
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute gyration tensor of.
+
+    Returns
+    -------
+    c:  np.ndarray, shape=(traj.n_frames, 1), dtype=float64
+        Acylindricity of each frame of the trajectory.
+
+    """
+    pm = principal_moments(traj)
+    kappa2 = 1.5 * np.square(pm).sum(axis=1) / np.square(pm.sum(axis=1)) - 0.5
+    return kappa2
 
 def _compute_gyration_tensor_slow(traj):
     """Compute the gyration tensor of a trajectory. """
