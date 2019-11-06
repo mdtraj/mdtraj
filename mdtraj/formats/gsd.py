bcc932faaa8da871ba0dedf769934a7426dd7b7a
Alex Yang <ahy3nz@virginia.edu>
2019-11-06 17:04:24 -0600

    Update conda recipe for gsd, update credits and readme

--- mdtraj/formats/gsd.py
+++ mdtraj/formats/gsd.py
@@ -3,8 +3,7 @@
 #         Molecular Dynamics Trajectories.
 # Copyright 2012-2013 Stanford University and the Authors
 #
-# Authors: Christoph Klein
-# Contributors: Robert T. McGibbon
+# Authors: Alexander Yang
 #
 # MDTraj is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as
