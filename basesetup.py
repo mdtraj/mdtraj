85ccce6c1de1cc2a1ff3f843542c69b9ddc390d3
Martin K. Scherer <m.scherer@fu-berlin.de>
2020-02-28 13:30:38 +0100

    [basesetup] pass omp.h header in OpenMP detection

--- basesetup.py
+++ basesetup.py
@@ -163,10 +163,11 @@ exit(status)
     def _detect_openmp(self):
         self._print_support_start('OpenMP')
         extra_postargs = ['/openmp'] if self.msvc else ['-fopenmp']
-        hasopenmp = self.hasfunction('omp_get_num_threads()', extra_postargs=extra_postargs)
+        args = dict(extra_postargs=extra_postargs, include='<omp.h>')
+        hasopenmp = self.hasfunction('omp_get_num_threads()', **args)
         needs_gomp = False
         if not hasopenmp:
-            hasopenmp = self.hasfunction('omp_get_num_threads()', extra_postargs=extra_postargs, libraries=['gomp'])
+            hasopenmp = self.hasfunction('omp_get_num_threads()', libraries=['gomp'], **args)
             needs_gomp = hasopenmp
         self._print_support_end('OpenMP', hasopenmp)
         return hasopenmp, needs_gomp
