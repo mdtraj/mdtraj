0a1751d840b01dc7cb300fc58695badcaf8f7fe4
Robert T. McGibbon <rmcgibbo@gmail.com>
2022-01-01 18:23:53 -0500

    [CI] Prepare for python310, add flake to manage nixpkgs version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except ValueError:
 
 
 ##########################
-VERSION = "1.9.8.dev0"
+VERSION = "1.9.8.dev0"  # please keep this in-sync with default.nix
 ISRELEASED = False
 __version__ = VERSION
 ##########################
