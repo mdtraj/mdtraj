a3a1d61b76a976a69076f8400b693ce67e791465
Robert T. McGibbon <rmcgibbo@gmail.com>
2022-02-08 21:26:07 -0500

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:rmcgibbo/nixpkgs/merge";
-  # convert back to nixos-unstable when https://github.com/NixOS/nixpkgs/pull/153116 goes through
-  # and https://github.com/NixOS/nixpkgs/pull/153108
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.py-utils.url = "github:rmcgibbo/python-flake-utils";
   inputs.utils.url = "github:numtide/flake-utils";
 
