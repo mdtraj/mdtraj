a6706ce754d3a2c3d4f4f3d029ab8e803b00b86e
Jeremy Leung <jml230@pitt.edu>
2024-10-03 11:22:16 -0400

    separate each pdb into its own test, allow 'P' to ' ' assignments

--- tests/test_dssp.py
+++ tests/test_dssp.py
@@ -26,13 +26,15 @@ def call_dssp(dirname, traj, frame=0):
     with open(out) as f:
         # exaust the first entries
         max(itertools.takewhile(lambda line: not line.startswith(KEY_LINE), f))
-        return np.array([line[16] for line in f if line[13] != "!"])
+        return np.array([line[16] for line in f if line[13] != "!"], dtype='<U1')
 
 
 def assert_(a, b):
     try:
         assert np.all(a == b)
     except AssertionError:
+        flag = False  # Flag to see if it's a true error or not (e.g. not PPII)
+
         if len(a) != len(b):
             print("Not the same length: %d vs %s" % (len(a), len(b)))
             raise
@@ -40,34 +42,43 @@ def assert_(a, b):
         for i, (aa, bb) in enumerate(zip(a, b)):
             if aa == bb:
                 print("%3d: '%s' '%s'" % (i, aa, bb))
+            elif aa == 'P' and bb == ' ':
+                # Bypassing cases where mkdssp outputs PPII ("P") and mdtraj doesn't.
+                print("%3d: '%s' '%s'" % (i, aa, bb))
             else:
                 print("%3d: '%s' '%s' <-" % (i, aa, bb))
-        raise
+                flag = True
+
+        if flag:
+            raise
 
 
 @needs_dssp
-def test_1(get_fn, tmpdir):
-    for fn in ["1bpi.pdb", "1vii.pdb", "4ZUO.pdb", "1am7_protein.pdb"]:
-        t = md.load_pdb(get_fn(fn))
-        t = t.atom_slice(t.top.select_atom_indices("minimal"))
-        assert_(call_dssp(tmpdir, t), md.compute_dssp(t, simplified=False)[0])
+@pytest.mark.parametrize('fn', ["1bpi.pdb", "1vii.pdb", "4ZUO.pdb", "1am7_protein.pdb"])
+def test_1(get_fn, tmpdir, fn):
+    """This test checks dssp assignments for pdb files in tests/data"""
+    t = md.load_pdb(get_fn(fn))
+    t = t.atom_slice(t.top.select_atom_indices("minimal"))
+    assert_(call_dssp(tmpdir, t), md.compute_dssp(t, simplified=False)[0])
 
 
 @needs_dssp
-def test_2(get_fn, tmpdir):
-    t = md.load(get_fn("2EQQ.pdb"))
+@pytest.mark.parametrize('fn', ["2EQQ.pdb"])
+def test_2(get_fn, tmpdir, fn):
+    """This test checks dssp assignments on different chains for pdb files in tests/data"""
+    t = md.load(get_fn(fn))
     for i in range(len(t)):
         assert_(call_dssp(tmpdir, t[i]), md.compute_dssp(t[i], simplified=False)[0])
 
 
 @needs_dssp
-def test_3(tmpdir):
+@pytest.mark.parametrize('pdbid', ["1GAI", "6gsv", "2AAC"])
+def test_3(tmpdir, pdbid):
+    """This test checks dssp assignments on pdb files downloaded from rcsb"""
     # 1COY gives a small error, due to a broken chain.
-    pdbids = ["1GAI", "6gsv", "2AAC"]
-    for pdbid in pdbids:
-        t = md.load_pdb("http://www.rcsb.org/pdb/files/%s.pdb" % pdbid)
-        t = t.atom_slice(t.top.select_atom_indices("minimal"))
-        assert_(call_dssp(tmpdir, t), md.compute_dssp(t, simplified=False)[0])
+    t = md.load_pdb("http://www.rcsb.org/pdb/files/%s.pdb" % pdbid)
+    t = t.atom_slice(t.top.select_atom_indices("minimal"))
+    assert_(call_dssp(tmpdir, t), md.compute_dssp(t, simplified=False)[0])
 
 
 def test_4(get_fn):
