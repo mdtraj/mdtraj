a33917929ebaa8802236fd3956b6729a0cc3f533
Jeremy M. G. Leung <jml230@pitt.edu>
2025-01-17 12:56:11 -0500

    add testing for overflow mode

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -382,6 +382,7 @@ def test_chimera_indexing_skip(get_fn):
     test_pos = np.array([[10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686], [10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686]], dtype=np.float32)
     assert np.array_equal(traj._xyz[0], test_pos)
 
+
 def test_vmd_indexing(get_fn):
     traj = load_pdb(get_fn("vmd_indexing.pdb"))  # this should just not fail
 
@@ -398,3 +399,19 @@ def test_vmd_indexing(get_fn):
 
     test_pos = np.array([[10.613, 0.225, 12.764], [10.613, 0.225, 12.764], [10.613, 0.225, 12.764], [10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686], [10.613, 0.225, 12.764], [10.629, 0.313, 12.729]], dtype=np.float32)
     assert np.array_equal(traj._xyz[0], test_pos)
+
+
+def test_overflow_indexing(get_fn):
+    traj = load_pdb(get_fn("overflow_indexing.pdb"))  # this should just not fail
+
+    assert traj.n_atoms == 9
+    assert traj.n_residues == 3
+    assert traj.topology._atoms[3].serial == 100000
+    assert traj.topology._atoms[3].residue.resSeq == 10000
+    assert traj.topology.n_bonds == 6
+    assert traj.topology._atoms[6].serial == 100003
+    assert traj.topology._atoms[6].residue.resSeq == 10001
+
+    test_pos = np.array([[10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686], [10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686], [10.613, 0.225, 12.764], [10.629, 0.313, 12.729], [10.596, 0.172, 12.686]], dtype=np.float32)
+    assert np.array_equal(traj._xyz[0], test_pos)
+
