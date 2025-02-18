f0437e359da087b0bb2d5685802589fc62ac2142
Ryan DiRisio <rdirisio@treeline.bio>
2025-02-18 18:00:49 -0500

    Add more tests

--- tests/test_pi_stacking.py
+++ tests/test_pi_stacking.py
@@ -58,13 +58,13 @@ def test_pi_stacking(get_fn):
     stacking_interactions = pi_stacking(t, lig_grps, protein_grps)
     # Since it returns per-frame, just get the 'first'
     stacking_interactions = stacking_interactions[0]
-    assert len(stacking_interactions == 2 )
+    assert len(stacking_interactions ) == 2
     assert (lig_grps[0], protein_grps[0]) in stacking_interactions
     assert (lig_grps[1], protein_grps[0]) in stacking_interactions
     assert (lig_grps[2], protein_grps[0]) not in stacking_interactions
 
 
-def test_t_stacking(get_fn):
+def test_t_stacking_more_relaxed_radius(get_fn):
     """Test that t-stacking version of pi-stacking is being detected correctly."""
     t: md.Trajectory = md.load(get_fn("4BFQ.pdb"))
     lig_aromatic_atms_to_test = [
@@ -93,11 +93,54 @@ def test_t_stacking(get_fn):
                 )
             )
         )
-    # More permissive edge intersection radius
+    # More permissive edge intersection radius for t-stack
     stacking_interactions = pi_stacking(
         t, lig_grps, protein_grps, edge_intersection_radius=0.21
     )
     stacking_interactions = stacking_interactions[0]
-    assert len(stacking_interactions == 1)
+    assert len(stacking_interactions) == 1
+    assert (lig_grps[0], protein_grps[0]) in stacking_interactions
+    assert (lig_grps[1], protein_grps[0]) not in stacking_interactions
+
+
+def test_t_stacking_default_settings(get_fn):
+    """Test that t-stacking version of pi-stacking is being detected correctly."""
+    
+    t: md.Trajectory = md.load(get_fn("6A22.pdb"))
+    # for atm in t.top.atoms:
+    #     print(atm.residue.chain.index, atm)
+
+    lig_aromatic_atms_to_test = [
+        ("S10", "C9", "C8","N7", "C6"),  # t-stacking
+        ("C14", "C15", "C16", "C17", "C18" ,"C19" ),  # NOT pi-stacking
+    ]
+    lig_grps = []
+    for lig_grp in lig_aromatic_atms_to_test:
+        lig_grps.append(
+            tuple(
+                int(idx)
+                for idx in t.top.select(
+                    f"chainid 9 and resSeq 9000 and (name {lig_grp[0]} or name {' or name '.join(lig_grp[1:])})"
+                )
+            )
+        )
+    protein_grps = []
+    # PHR118/378
+    protein_stacking_atms = [("CE2", "CD2", "CG", "CD1", "CE1", "CZ")]
+    for protein_grp in protein_stacking_atms:
+        protein_grps.append(
+            tuple(
+                int(idx)
+                for idx in t.top.select(
+                    f"chainid 2 and resSeq 378 and (name {protein_grp[0]} or name {' or name '.join(protein_grp[1:])})"
+                )
+            )
+        )
+    # More permissive edge intersection radius for t-stack
+    stacking_interactions = pi_stacking(
+        t, lig_grps, protein_grps
+    )
+    stacking_interactions = stacking_interactions[0]
+    assert len(stacking_interactions) == 1
     assert (lig_grps[0], protein_grps[0]) in stacking_interactions
     assert (lig_grps[1], protein_grps[0]) not in stacking_interactions
