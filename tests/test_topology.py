4788995cec889c9cebbdf91c871f63c5a9f4cfb7
Lex O'brien <lex.e.obrien@gmail.com>
2025-05-19 17:11:13 +0100

    bugfix: delete_atom_by_index updates bond list (#2021)

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -430,3 +430,28 @@ def test_topology_join_keep_resSeq(get_fn):
 
     eq(out_resSeq_keepId_True, expected_resSeq_keepId_True)
     eq(out_resSeq_keepId_False, expected_resSeq_keepId_False)
+
+def test_delete_atom_by_index(get_fn):
+    """
+REMARK *                                                                              
+REMARK   DATE:     8/ 5/ 9     14:44:19      CREATED BY USER: mjw                     
+ATOM      1  N   ALA     1       0.024  -0.103  -0.101  1.00  0.00      AAL 
+ATOM      2  HT1 ALA     1       0.027  -1.132  -0.239  1.00  0.00      AAL 
+ATOM      3  HT2 ALA     1      -0.805   0.163   0.471  1.00  0.00      AAL 
+ATOM      4  HT3 ALA     1      -0.059   0.384  -1.019  1.00  0.00      AAL 
+ATOM      5  CA  ALA     1       1.247   0.375   0.636  1.00  0.00      AAL 
+...
+    """
+    index=0
+    top = md.load(get_fn("ala_ala_ala.pdb")).topology
+    n_bonds_original = top.n_bonds
+    n_atoms_original = top.n_atoms
+    atom_to_remove = top._atoms[index]
+    top.delete_atom_by_index(index)
+    assert top.n_atoms == n_atoms_original - 1
+    assert top.n_bonds == n_bonds_original - 4
+    assert all([atom_to_remove not in bond for bond in top.bonds])
+    assert eq(list(range(top.n_atoms)), [atom.index for atom in top.atoms]) 
+
+
+    
