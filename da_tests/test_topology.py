fd3d39735568a4e6f4aa8830e6d9009c56107d87
Lex O'brien <lex.e.obrien@gmail.com>
2025-05-24 21:08:20 +0100

    removed scratch tests

--- da_tests/test_topology.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import mdtraj as md
-
-
-def test_delete_atom_by_index_deprecated(get_fn):
-    top = md.load(get_fn("ala_ala_ala.pdb")).topology
-    with pytest.deprecated_call(): 
-        top.delete_atom_by_index(0)
