6b59e16f2f9362b9b99bc2313d1136688f6516a5
Jessica A. Nash <janash@vt.edu>
2025-06-04 13:46:23 -0400

    add formal charge in missing places on topology (#2038)

--- mdtraj/core/topology.py
+++ mdtraj/core/topology.py
@@ -125,6 +125,7 @@ def _topology_from_subset(topology: Topology, atom_indices: list[int]) -> Topolo
                         atom.element,
                         newResidue,
                         serial=serial,
+                        formal_charge=atom.formal_charge,
                     )
                     old_atom_to_new_atom[atom] = newAtom
 
@@ -258,7 +259,7 @@ class Topology:
             for residue in chain.residues:
                 r = out.add_residue(residue.name, c, residue.resSeq, residue.segment_id)
                 for atom in residue.atoms:
-                    out.add_atom(atom.name, atom.element, r, serial=atom.serial)
+                    out.add_atom(atom.name, atom.element, r, serial=atom.serial, formal_charge=atom.formal_charge)
 
         for bond in self.bonds:
             a1, a2 = bond
@@ -318,12 +319,7 @@ class Topology:
                     out_resSeq += 1
                 r = out.add_residue(residue.name, c, out_resSeq, residue.segment_id)
                 for atom in residue.atoms:
-                    a = out.add_atom(
-                        atom.name,
-                        atom.element,
-                        r,
-                        serial=atom.serial,
-                    )
+                    a = out.add_atom(atom.name, atom.element, r, serial=atom.serial, formal_charge=atom.formal_charge)
                     atom_mapping[atom] = a
 
         for bond in other.bonds:
@@ -1779,7 +1775,7 @@ class Atom:
         # The not-necessarily-contiguous "serial" number from the PDB spec
         self.serial: int | None = serial
         # The formal charge of the atom
-        self.formal_charge: float | None = formal_charge
+        self.formal_charge: int | None = formal_charge
 
     @property
     def n_bonds(self) -> int:
@@ -1809,6 +1805,8 @@ class Atom:
         if self.name != other.name:
             return False
         if self.index != other.index:
+            return False
+        if self.formal_charge != other.formal_charge:
             return False
         if self.element.name != other.element.name:
             return False
