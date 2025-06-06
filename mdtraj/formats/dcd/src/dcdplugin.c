be5873bd8d6a3921e7ced84b0b890f11d0e17f7f
Benjamin Pritchard <ben@bennyp.org>
2025-06-06 11:50:42 -0400

    Use info in dcdplugin.h header rather than copy to source

--- mdtraj/formats/dcd/src/dcdplugin.c
+++ mdtraj/formats/dcd/src/dcdplugin.c
@@ -55,65 +55,7 @@
 #include <time.h>
 #include "endianswap.h"
 #include "molfile_plugin.h"
-
-#ifndef M_PI_2
-#define M_PI_2 1.57079632679489661922
-#endif
-
-#define RECSCALE32BIT 1
-#define RECSCALE64BIT 2
-#define RECSCALEMAX   2
-
-typedef struct {
-  fio_fd fd;
-  int natoms;
-  int nsets;
-  int setsread;
-  int istart;
-  int nsavc;
-  double delta;
-  int nfixed;
-  float *x, *y, *z;
-  int *freeind;
-  float *fixedcoords;
-  int reverse;
-  int charmm;
-  int first;
-  int with_unitcell;
-} dcdhandle;
-
-/* Define error codes that may be returned by the DCD routines */
-#define DCD_SUCCESS      0  /* No problems                     */
-#define DCD_EOF         -1  /* Normal EOF                      */
-#define DCD_DNE         -2  /* DCD file does not exist         */
-#define DCD_OPENFAILED  -3  /* Open of DCD file failed         */
-#define DCD_BADREAD     -4  /* read call on DCD file failed    */
-#define DCD_BADEOF      -5  /* premature EOF found in DCD file */
-#define DCD_BADFORMAT   -6  /* format of DCD file is wrong     */
-#define DCD_FILEEXISTS  -7  /* output file already exists      */
-#define DCD_BADMALLOC   -8  /* malloc failed                   */
-#define DCD_BADWRITE    -9  /* write call on DCD file failed   */
-
-/* Define feature flags for this DCD file */
-#define DCD_IS_XPLOR        0x00
-#define DCD_IS_CHARMM       0x01
-#define DCD_HAS_4DIMS       0x02
-#define DCD_HAS_EXTRA_BLOCK 0x04
-#define DCD_HAS_64BIT_REC   0x08
-
-/* defines used by write_dcdstep */
-#define NFILE_POS 8L
-#define NSTEP_POS 20L
-
-/* READ Macro to make porting easier */
-#define READ(fd, buf, size)  fio_fread(((void *) buf), (size), 1, (fd))
-
-/* WRITE Macro to make porting easier */
-#define WRITE(fd, buf, size) fio_fwrite(((void *) buf), (size), 1, (fd))
-
-/* XXX This is broken - fread never returns -1 */
-#define CHECK_FREAD(X, msg) if (X==-1) { return(DCD_BADREAD); }
-#define CHECK_FEOF(X, msg)  if (X==0)  { return(DCD_BADEOF); }
+#include "dcdplugin.h"
 
 
 /* print DCD error in a human readable way */
@@ -990,7 +932,7 @@ dcdhandle* open_dcd_read(const char *path, const char *filetype,
 }
 
 
-int read_next_timestep(void *v, int natoms, molfile_timestep_t *ts) {
+int read_next_timestep(dcdhandle *v, int natoms, molfile_timestep_t *ts) {
   dcdhandle *dcd;
   int i, j, rc;
   float unitcell[6];
@@ -1071,7 +1013,7 @@ int read_next_timestep(void *v, int natoms, molfile_timestep_t *ts) {
 }
 
 
-void close_file_read(void *v) {
+void close_file_read(dcdhandle *v) {
   dcdhandle *dcd = (dcdhandle *)v;
   close_dcd_read(dcd->freeind, dcd->fixedcoords);
   fio_fclose(dcd->fd);
@@ -1083,7 +1025,7 @@ void close_file_read(void *v) {
 
 
 dcdhandle* open_dcd_write(const char *path, const char *filetype,
-    int natoms, const int with_unitcell) {
+    const int natoms, const int with_unitcell) {
   dcdhandle *dcd;
   fio_fd fd;
   int rc;
@@ -1136,7 +1078,7 @@ dcdhandle* open_dcd_write(const char *path, const char *filetype,
 }
 
 
-int write_timestep(void *v, const molfile_timestep_t *ts) {
+int write_timestep(dcdhandle *v, const molfile_timestep_t *ts) {
   dcdhandle *dcd = (dcdhandle *)v;
   int i, rc, curstep;
   float *pos = ts->coords;
@@ -1172,7 +1114,7 @@ int write_timestep(void *v, const molfile_timestep_t *ts) {
   return MOLFILE_SUCCESS;
 }
 
-void close_file_write(void *v) {
+void close_file_write(dcdhandle *v) {
   dcdhandle *dcd = (dcdhandle *)v;
   fio_fclose(dcd->fd);
   free(dcd->x);
